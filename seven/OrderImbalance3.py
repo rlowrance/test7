76588306c3265361088c999e485d178278f383d2
Roy Lowrance <roy.lowrance@gmail.com>
2017-02-16 18:12:55 -0500

    new order imbalance algo

--- seven/OrderImbalance3.py
+++ seven/OrderImbalance3.py
@@ -4,38 +4,41 @@ import pdb
 import unittest
 
 from MaybeNumber import MaybeNumber
-from Windowed import Windowed
 
 
 class OrderImbalance(object):
-    def __init__(self, lookback=None, typical_bid_offer=None):
-        assert lookback > 0
+    def __init__(self, typical_bid_offer=None):
         assert typical_bid_offer > 0
 
-        self.lookback = lookback
-        self.typical_bid_offer = typical_bid_offer
+        self.typical_bid_offer = MaybeNumber(typical_bid_offer)
 
-        self.prior_bid_price = None
-        self.prior_offer_price = None
-        self.bid_window = Windowed(lookback)
-        self.offer_window = Windowed(lookback)
+        self.prior_bid_price = MaybeNumber(None)
+        self.prior_offer_price = MaybeNumber(None)
+
+        self.cumulatively_bought = MaybeNumber(0)
+        self.cumulatively_sold = MaybeNumber(0)
 
     def p(self):
         'print self'
         format = '%30s: %s'
-        print format % ('lookback', self.lookback)
         print format % ('typical_bid_offer', self.typical_bid_offer)
         print format % ('prior_bid_price', self.prior_bid_price)
         print format % ('prior_offer_price', self.prior_offer_price)
-        print format % ('bid_window', self.bid_window)
-        print format % ('offer_window', self.offer_window)
+        print format % ('cumulatively_bought', self.cumulatively_bought)
+        print format % ('cumulatively_sold', self.cumulatively_sold)
 
     def open_interest(self, trade_type=None, trade_quantity=None, trade_price=None, verbose=False):
         'return a MaybeNumber, possibly containing the order imbalance, which may not exist'
-        def closer(x, a, b):
-            'return MaybeNumber(True) iff x is closer to a than b'
-            # print 'closer', x, a, b
-            return abs(MaybeNumber(x) - MaybeNumber(a)) < abs(MaybeNumber(x) - MaybeNumber(b))
+        def make_bid_weight(dealer_price, bid_price, offer_price):
+            'return weight to be given to bid as a MaybeNumber'
+            def distance(a, b):
+                diff = a - b
+                return (diff * diff).sqrt()
+
+            distance_to_bid = distance(dealer_price, bid_price)
+            distance_to_offer = distance(dealer_price, offer_price)
+            print dealer_price, bid_price, offer_price, distance_to_bid, distance_to_offer
+            return 1.0 - (distance_to_bid / (distance_to_bid + distance_to_offer))
 
         assert trade_type in ('B', 'D', 'S')
         assert trade_quantity > 0
@@ -44,52 +47,49 @@ class OrderImbalance(object):
         if verbose:
             self.p()
             print trade_type, trade_quantity, trade_price
-            pdb.set_trace()
 
         # update prior prices, using the current trade
         # accumulate trades in the window
+        price = MaybeNumber(trade_price)
+        quantity = MaybeNumber(trade_quantity)
         if trade_type == 'B':
             # dealer buy, hence a bid
-            self.prior_bid_price = trade_price
-            self.bid_window.append(trade_quantity)
-            if self.prior_offer_price is None:
-                self.prior_offer_price = self.prior_bid_price + self.typical_bid_offer  # or -?
+            self.prior_bid_price = price
+            self.cumulatively_bought += trade_quantity
+            if self.prior_offer_price.value is None:
+                self.prior_offer_price = self.prior_bid_price + self.typical_bid_offer
+                print self.prior_bid_price, self.prior_offer_price
+                assert self.prior_bid_price >= self.prior_offer_price  # because prices are OAS spreads
         elif trade_type == 'S':
             # dealer sell, hence an offer
-            self.prior_offer_price = trade_price
-            self.offer_window.append(trade_quantity)
-            if self.prior_bid_price is None:
-                self.prior_bid_price = self.prior_offer_price - self.typical_bid_offer  # or +?
+            self.prior_offer_price = price
+            self.cumulatively_sold += quantity
+            if self.prior_bid_price.value is None:
+                self.prior_bid_price = self.prior_offer_price - self.typical_bid_offer
         else:
             # classify the trade is a dealer buy or sell based on
             # how close it is to prior buys and sells
             assert trade_type == 'D'
-            if closer(trade_price, self.prior_bid_price, self.prior_offer_price):
-                self.prior_bid_price = trade_price
-                self.bid_window.append(trade_quantity)
-            elif closer(trade_price, self.prior_offer_price, self.prior_bid_price):
-                self.prior_offer_price = trade_price
-                self.offer_window.append(trade_quantity)
+            if price.ge(self.prior_bid_price).value:
+                weight_bid = MaybeNumber(1.0)
+            elif price.le(self.prior_offer_price).value:
+                weight_bid = MaybeNumber(0.0)
             else:
-                # Q: Should we instead just discard this kind of trade?
-                # the trade is equal distance from the most recent buy and sell
-                # classify it based on the synthetic mid price
-                synthetic_mid_price = (self.prior_bid_price + self.prior_offer_price) / 2.0
-                if trade_price < synthetic_mid_price:
-                    self.prior_bid_price = trade_price
-                    self.bid_window.append(trade_quantity)
-                else:
-                    self.prior_offer_price = trade_price
-                    self.offer_window.append(trade_quantity)
+                weight_bid = make_bid_weight(price, self.prior_bid_price, self.prior_offer_price)
+            if verbose:
+                print 'weight_bid', weight_bid
+            if weight_bid.value is not None:
+                self.cumulatively_bought += weight_bid * quantity
+                self.cumulatively_sold += (1.0 - weight_bid) * quantity
+                # NOTE: do not update prior bid and offer prices
+            else:
+                # throw away the dealer trade_price because we cannot determine weight_bid
+                # note that the cumulative_bought and sold will not change and that they
+                # were initialized to zero, leading to an error when the first
+                # trade is a D
+                pass
 
-        # classify dealer trade as buy or sell
-        # based on how close it is to prior buy and sell prices and
-        # its price relative to the synthetic mid price
-
-        # Q: Should we instead go back to the issuance of the bond instead of the last LOOKBACK trades?
-        # Q: should there be a weighting to the result (in the feature space)
-        # Q: let's review the orcl sample1 trades and see if the derived order imbalance numbers are reasonable
-        result = self.bid_window.sum() - self.offer_window.sum()  # a MaybeNumber
+        result = self.cumulatively_bought - self.cumulatively_sold  # a MaybeNumber
         if verbose:
             self.p()
             print result
@@ -98,8 +98,8 @@ class OrderImbalance(object):
 
 
 class TestOrderImbalance(unittest.TestCase):
-    def common_tester(self, lookback, typical_bid_offer, tests, debug=False):
-        oi = OrderImbalance(lookback=lookback, typical_bid_offer=typical_bid_offer)
+    def common_tester(self, typical_bid_offer, tests, debug=False):
+        oi = OrderImbalance(typical_bid_offer=typical_bid_offer)
         for test in tests:
             trade_type, trade_quantity, trade_price, expected_imbalance = test
             actual_imbalance = oi.open_interest(trade_type, trade_quantity, trade_price)
@@ -112,24 +112,25 @@ class TestOrderImbalance(unittest.TestCase):
 
     def test_make_order_imbalance_start_with_B(self):
         debug = False
-        lookback = 1
         typical_bid_offer = 2
         tests = (  # tests are applied sequentially to a single OrderImbalance instance
             ('B', 1, 101, 1),
             ('B', 2, 102, 3),
             ('S', 3, 100, 0),
-            ('D', 4, 101, 4),  # get's classified as a B
+            ('D', 4, 101, 0),    # allocated .50-.50, since spread is (102, 100)
+            ('D', 5, 100, -5),   # allocated entirely as a S
+            ('D', 6, 103, 1),    # allocated entirely as B
+            ('D', 7, 99, -6),    # allocated entire as S
+            ('D', 8, 100.5, -10),  # allocat3ed 2 to B, 6 to S
         )
-        self.common_tester(lookback, typical_bid_offer, tests, debug)
+        self.common_tester(typical_bid_offer, tests, debug)
 
     def test_make_order_imbalance_start_with_D(self):
         debug = False
-        lookback = 1
         typical_bid_offer = 2
         tests = (  # tests are applied sequentially to a single OrderImbalance instance
-            ('D', 4, 101, None),  # get's classified as a B
         )
-        self.common_tester(lookback, typical_bid_offer, tests, debug)
+        self.common_tester(typical_bid_offer, tests, debug)
 
 
 if __name__ == '__main__':
