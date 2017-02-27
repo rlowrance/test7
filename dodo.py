41a875d8fa0bc3afc2ebeb705bd4f17ea8f96db1
Roy Lowrance <roy.lowrance@gmail.com>
2017-02-27 18:16:30 -0500

    add task targets; refactor

--- dodo.py
+++ dodo.py
@@ -1,6 +1,16 @@
+# invocations
+#  doit help
+#  doit help run
+#  doit list
+#  doit cusips:filename*  # must contain a star, leave out the task_ prefix
+#  doit -n 3  # run 3 processes
+import pdb
+from pprint import pprint
+
 import cusips
 import features
 import seven.path
+import targets
 
 DOIT_CONFIG = {
     'check_file_uptodate': 'md5',
@@ -13,41 +23,42 @@ working = seven.path.working()
 def show_command(task):
     return 'executing actions for task %s' % task.name
 
-common_return = {
-    'title': show_command,
-    'verbosity': 2,
-}
 
-
-def program_cusips(ticker):
-    doit = cusips.Doit(ticker)
-    return common_return.update({
-        'actions': doit.actions,
-        'targets': doit.targets,
-        'file_dep': doit.file_dep,
-    })
-
-
-def program_features(ticker):
-    doit = features.Doit(ticker)
-    return common_return.update({
-        'actions': doit.actions,
-        'targets': doit.targets,
-        'file_dep': doit.file_dep,
-    })
-
-
-def task_cusips_orcl():
-    return program_cusips('orcl')
+def make_task_result(doit_value):
+    result = {
+        'title': show_command,
+        'verbosity': 2,
+        'actions': doit_value.actions,
+        'targets': doit_value.targets,
+        'file_dep': doit_value.file_dep,
+    }
+    return result
 
 
 def task_cusips_msft():
-    return program_cusips('msft')
+    return make_task_result(cusips.Doit('msft'))
 
 
-def task_features_orcl():
-    return program_features('orcl')
+def task_cusips_orcl():
+    return make_task_result(cusips.Doit('orcl'))
 
 
 def task_features_msft():
-    return program_features('msft')
+    return make_task_result(features.Doit('msft'))
+
+
+def task_features_orcl():
+    return make_task_result(features.Doit('orcl'))
+
+
+def task_targets_msft():
+    return make_task_result(targets.Doit('msft'))
+
+
+def task_targets_orcl():
+    return make_task_result(targets.Doit('orcl'))
+
+
+if False:  # avoid pyflakes warnings
+    pdb
+    pprint
