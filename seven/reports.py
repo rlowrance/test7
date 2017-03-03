c21be884171cbe3562efa50082cd1ddd8d4b48da
Roy Lowrance <roy.lowrance@gmail.com>
2017-03-02 20:50:49 -0500

    additional columns

--- seven/reports.py
+++ seven/reports.py
@@ -3,18 +3,21 @@ import pdb
 import pickle_utilities
 
 all_columns = {
-    'ticker': (6, '%6s', ('', 'ticker'), 'identifies a particular security'),
     'cusip': (9, '%9s', ('', 'cuisp'), 'identifies a particular security'),
+    'feature_importance': (4, '%4.2f', ('feat', 'impt'), 'feature importance (fraction of decision trees using the feature)'),
+    'feature_name': (20, '%20s', ('feature', 'name'), 'name of features'),
+    'loss': (9, '%9.2f', ('', 'loss'), 'loss'),
+    'max_loss': (9, '%9.2f', ('max', 'loss'), 'maximum loss'),
     'model_name': (5, '%5s', ('model', 'name'), 'name of model'),
-    'min_loss': (9, '%9.2f', ('min', 'loss'), 'minimum loss'),
+    'mean_feature_importance': (4, '%4.2f', ('mean', 'fimp'), 'mean of feature importance (fraction of decision trees using the feature)'),
     'mean_loss': (9, '%9.2f', ('mean', 'loss'), 'mean loss'),
     'median_loss': (9, '%9.2f', ('median', 'loss'), 'median loss'),
-    'max_loss': (9, '%9.2f', ('max', 'loss'), 'maximum loss'),
+    'min_loss': (9, '%9.2f', ('min', 'loss'), 'minimum loss'),
+    'model_spec': (20, '%-20s', ('model', 'spec'), 'model specification'),
     'n_hp_sets': (7, '%7d', ('number', 'hp sets'), 'number of hyperparameter sets'),
     'n_samples': (7, '%7d', ('number', 'samples'), 'number of samples'),
     'std_loss': (9, '%9.2f', ('std', 'loss'), 'standard deviation of loss'),
-    'loss': (9, '%9.2f', ('', 'loss'), 'loss'),
-    'model_spec': (20, '%-20s', ('model', 'spec'), 'model specification'),
+    'ticker': (6, '%6s', ('', 'ticker'), 'identifies a particular security'),
 }
 
 
