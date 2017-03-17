932b8aaaf5731eda5ad844ebd0fd9cda4899cb96
Roy Lowrance <roy.lowrance@gmail.com>
2017-03-17 07:59:16 -0400

    price delta ratios (and other)

--- features.py
+++ features.py
@@ -20,6 +20,18 @@ OUTPUTS
  WORKING/features-{ticker}/{ticker}-{cusip}.csv
 where
  {cusip} is in the {ticker} file
+
+FEATURES CREATED BY INPUT FILE AND NEXT STEPS (where needed)
+ {ticker}.csv: trade by effectivedatetime
+  order_imbalance4
+  prior_price_{trade_type}       convert price to spread
+  prior_quantity_{trade_type}
+  trade_price                    convert price to spread
+  trade_quantity
+  trade_type_is_{trade_type}
+
+for
+ trade_type in {B, D, S}
 '''
 
 from __future__ import division
@@ -61,8 +73,10 @@ class Doit(object):
         # read in CUSIPs for the ticker
         with open(os.path.join(working, 'cusips', ticker + '.pickle'), 'r') as f:
             self.cusips = pickle.load(f).keys()
-        # path to files abd durecties
+        # path to files and durecties
+        self.in_ticker_equity_ohlc = os.path.join(midpredictor, ticker + '_equity_ohlc.csv')
         self.in_security_master = os.path.join(midpredictor, ticker + '_sec_master.csv')
+        self.in_spx_equity_ohlc = os.path.join(midpredictor, 'spx_equity_ohlc.csv')
         self.in_ticker_filename = os.path.join(midpredictor, ticker + '.csv')
         self.out_cusips = [
             os.path.join(out_dir, '%s-%s.csv' % (ticker, cusip))
@@ -79,7 +93,9 @@ class Doit(object):
             self.targets.append(cusip)
         self.file_dep = [
             self.me + '.py',
+            self.in_ticker_equity_ohlc,
             self.in_security_master,
+            self.in_spx_equity_ohlc,
             self.in_ticker_filename,
         ]
 
@@ -187,7 +203,7 @@ class Skipped(object):
         self.n_skipped += 1
 
 
-class Context(object):
+class ContextCusip(object):
     'accumulate running info for trades for a specific CUSIP'
     def __init__(self, lookback=None, typical_bid_offer=None, proximity_cutoff=None):
         self.order_imbalance4_object = OrderImbalance4(
@@ -231,6 +247,59 @@ class Context(object):
         )
 
 
+class DeltaPrice(object):
+    'ratios of delta ticker / delta spx for closing prices'
+    def __init__(self, df_ticker=None, df_spx=None):
+        'precompute all results'
+        test = False
+        self.days_back = (1, 2, 3, 5, 20)
+        self.available_dates = set()
+        history_len = 1 + 20  # 1 plus max lookback period (1 month == 20 trade days, ignoring holidays)
+        closes_ticker = collections.deque([], history_len)
+        closes_spx = collections.deque([], history_len)
+        self.ratios = {}  # Dict[(date, days_back), ratio:float]
+        for timestamp in sorted(df_ticker.index):
+            ticker = df_ticker.loc[timestamp]
+            if timestamp.date() not in df_spx.index:
+                print 'skipping index %s is in ticker but not spx' % timestamp
+                continue
+            spx = df_spx.loc[timestamp]
+            if test:
+                print timestamp
+                print ticker
+                print spx
+            closes_ticker.append(ticker.Close)
+            closes_spx.append(spx.Close)
+            if len(closes_ticker) >= history_len:
+                # constraint is to be able to determine equity_price_delta_close_prior_month
+                # CHECK: OK to define week at 5 days ago and month as 20 days ago
+                date = timestamp.date()
+                assert date not in self.available_dates
+                self.available_dates.add(date)
+                for days_back in self.days_back:
+                    day_index = 1 + days_back
+                    delta_ticker = closes_ticker[-day_index] * 1.0 / ticker.Close
+                    delta_spx = closes_spx[-day_index] * 1.0 / spx.Close
+                    ratio = delta_ticker / delta_spx
+                    self.ratios[(date, days_back)] = ratio
+        if test:
+            count = 0
+            for date in self.available_dates:
+                for days_back in self.days_back:
+                    print date, days_back, self.ratio(date, days_back)
+                count += 1
+                if count > 2:
+                    break
+
+    def is_available(self, date):
+        return date in self.available_dates
+
+    def ratio(self, date, days_back):
+        assert date in self.available_dates
+        assert days_back in self.days_back
+        return self.ratios[(date, days_back)]
+
+
 def do_work(control):
     'write order imbalance for each trade in the input file'
     def validate_trades(df):
@@ -259,14 +328,14 @@ def do_work(control):
     verbose = False
 
     # read {ticker}.csv
-    df_trades = models.read_csv(
+    df_ticker = models.read_csv(
         control.doit.in_ticker_filename,
         parse_dates=['maturity', 'effectivedate', 'effectivetime'],
         nrows=1000 if control.arg.test else None,
         verbose=True,
     )
-    validate_trades(df_trades)
-    df_trades['effectivedatetime'] = models.make_effectivedatetime(df_trades)
+    validate_trades(df_ticker)
+    df_ticker['effectivedatetime'] = models.make_effectivedatetime(df_ticker)
 
     df_security_master = models.read_csv(
         control.doit.in_security_master,
@@ -276,8 +345,20 @@ def do_work(control):
     print len(df_security_master)
     df_security_master['cusip'] = df_security_master.index
 
-    result = {}   # Dict[cusip, pd.DataFrame]
-    context = {}  # Dict[cusip, Context]
+    def read_equity_ohlc(path):
+        return models.read_csv(
+            path,
+            parse_dates=[0],
+            verbose=True
+        )
+
+    delta_price = DeltaPrice(
+        df_ticker=read_equity_ohlc(control.doit.in_ticker_equity_ohlc),
+        df_spx=read_equity_ohlc(control.doit.in_spx_equity_ohlc),
+    )
+
+    result = {}          # Dict[cusip, pd.DataFrame]
+    context_cusip = {}   # Dict[cusip, ContextCusip]
     order_imbalance4_hps = {
         'lookback': 10,
         'typical_bid_offer': 2,
@@ -287,10 +368,10 @@ def do_work(control):
     count = 0
     cusips_not_in_security_master = set()
     maturity_dates_bad = set()
-    for index, trade in df_trades.iterrows():
+    for index, trade in df_ticker.iterrows():
         count += 1
         if count % 1000 == 1:
-            print 'trade count %d of %d' % (count, len(df_trades))
+            print 'trade count %d of %d' % (count, len(df_ticker))
         if verbose:
             print 'index', index, trade.cusip, trade.trade_type
         cusip = trade.cusip
@@ -298,14 +379,14 @@ def do_work(control):
             if cusip != control.arg.cusip:
                 print 'skipping cusip %s, because of --cusip arg' % cusip
                 continue
-        if cusip not in context:
-            context[cusip] = Context(
+        if cusip not in context_cusip:
+            context_cusip[cusip] = ContextCusip(
                 lookback=order_imbalance4_hps['lookback'],
                 typical_bid_offer=order_imbalance4_hps['typical_bid_offer'],
                 proximity_cutoff=order_imbalance4_hps['proximity_cutoff'],
             )
             result[cusip] = pd.DataFrame()
-        cusip_context = context[cusip]
+        cusip_context = context_cusip[cusip]
         cusip_context.update(trade)
         if cusip_context.missing_any_historic_price():
             skipping(cusip, index, 'missing some historic prices')
@@ -314,6 +395,11 @@ def do_work(control):
             if cusip not in cusips_not_in_security_master:
                 skipping(cusip, index, 'not in security master')
                 cusips_not_in_security_master.add(cusip)
+            continue
+        # features from {ticker}_equity_ohlc and spx_equity_ohlc
+        trade_date = trade.effectivedate.date()
+        if not delta_price.is_available(trade_date):
+            skipping(cusip, index, 'delta_price not available for %s' % trade_date)
             continue
         # features from the security master file
         # TODO: receive version with an effectivedate, then add these features:
@@ -327,7 +413,7 @@ def do_work(control):
         is_callable = get_security_master(cusip, 'is_callable')
         maturity_date = get_security_master(cusip, 'maturity_date')
         coupon_type = get_security_master(cusip, 'CPN_TYP')
-        assert coupon_type in ('FIXED', 'FLOATING')
+        assert coupon_type in ('FIXED', 'FLOATING')  # TODO: add stepup and possibly others
         months_to_maturity = months_from_until(trade.effectivedate, maturity_date)
         if months_to_maturity < 0:
             if maturity_date not in maturity_dates_bad:
@@ -344,7 +430,13 @@ def do_work(control):
         next_row = models.make_features_dict(
             id_cusip=trade.cusip,
             id_effectivedatetime=trade.effectivedatetime,
-            # features from df_security_master
+            # features from {ticker}_equity_ohlc and spx_equity_ohlc
+            price_delta_ratio_1_day=delta_price.ratio(trade_date, 1),
+            price_delta_ratio_2_days=delta_price.ratio(trade_date, 2),
+            price_delta_ratio_3_days=delta_price.ratio(trade_date, 3),
+            price_delta_ratio_week=delta_price.ratio(trade_date, 5),
+            price_delta_ratio_month=delta_price.ratio(trade_date, 20),
+            # features from {ticker}_security_master
             amount_issued=amount_issued,
             collateral_type_is_sr_unsecured=collateral_type == 'SR UNSECURED',
             coupon_is_fixed=coupon_type == 'FIXED',
@@ -352,7 +444,7 @@ def do_work(control):
             coupon_current=get_security_master(cusip, 'curr_cpn'),
             is_callable=is_callable == 'TRUE',
             months_to_maturity=months_to_maturity,
-            # features from df_trades
+            # features from df_ticker
             order_imbalance4=cusip_context.order_imbalance4,
             prior_price_B=cusip_context.prior_price_B,
             prior_price_D=cusip_context.prior_price_D,
@@ -397,7 +489,7 @@ def main(argv):
     lap('work completed')
     if control.arg.test:
         print 'DISCARD OUTPUT: test'
-    print control
+    # print control
     print 'done'
     return
 
