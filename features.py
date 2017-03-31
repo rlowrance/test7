0282063fe0832406ba87632c3531072a8d7b2c0e
Roy Lowrance <roy.lowrance@gmail.com>
2017-03-31 17:02:39 -0400

    add otr features

--- features.py
+++ features.py
@@ -1,18 +1,18 @@
 '''create feature sets for a CUSIP
 
 INVOCATION
-  python features.py {ticker}.csv [--cusip CUSIP] [--test] [--trace]
+  python features.py {ticker}.csv [--cusips CUSIP ...] [--test] [--trace]
 
 where
  {ticker}.csv is a CSV file in MidPredictors/data
- --cusip CUSIP means to create the targets only for the specified CUSIP
+ --cusips CUSIP1 CUSIP2 ... means to create the targets only for the specified CUSIPs
  --test means to set control.test, so that test code is executed
  --trace means to invoke pdb.set_trace() early in execution
 
 EXAMPLES OF INVOCATION
  python features.py orcl
- python features.py orcl --cusip 68389XAS4  --test
- python features.py orcl --cusip 68389XAP0  --test
+ python features.py orcl --cusips 68389XAS4 --test   (NOTE: does not create output)
+ python features.py orcl --cusips 68389XAC9 --test   (creates 137 feature output records )
 
 INPUTS
  MidPredictor/{ticker}.csv
@@ -26,6 +26,7 @@ FEATURES CREATED BY INPUT FILE AND NEXT STEPS (where needed)
   identifiers (not features) all from the {ticker}.csv file
     id_index              index from the {ticker}.csv file; this uniquely identifies the ticker_record
     id_cusip              cusip
+    id_cusip_otr          related on-the-run cusip
     id_effectivedatetime  date & time of the ticker_record
 
   comparables_{ticker}.csv    TODO GC to provide file with time stamps and related CUSIPs
@@ -34,14 +35,16 @@ FEATURES CREATED BY INPUT FILE AND NEXT STEPS (where needed)
     oftr_{oftr_cardinal}-{feature}   missing if bond is on the run; redefine feature sets when missing
     <other features to be designed by GC>
 
-  {ticker}.csv: ticker_record info by effectivedatetime for many CUSIPs in the file
-    oasspread_size
-    order_imbalance4
-    oasspread_{bds}_size
-    quantity_{bds}_size
-    quantity_size
-    trade_type_is_{bds}
+  nodupe_trace_{ticker}_otr.csv: ticker_record info with unique key issuepriceid  for all CUSIPs in the file
+    p_oasspread_size
+    p_order_imbalance4
+    p_oasspread_{bds}_size
+    p_quantity_{bds}_size
+    p_quantity_size
+    p_trade_type_is_{bds}
     where {bds} is one of {'B', 'D', 'S'}
+    NOTE: this file contains cusip1, which is the cusip for the on-the-run bond that most trades like
+          the bond identified in the field cusip
 
   {ticker}_etf_{index_name}.csv
     etf_weight_{index_name}_size  number in [0, 1]
@@ -57,7 +60,7 @@ FEATURES CREATED BY INPUT FILE AND NEXT STEPS (where needed)
     gross_leverage
     interest_coverage
     (interest_expense_size)           TODO: add once appears in file
-    ltm_ebitda_size                   NOTE: Googe doc asks for last 12 months EBITDA
+    ltm_ebitda_size
     (operating_income_size)           TODO: add once appears in file
 
   {ticker}_and_comps_sec_master.csv   GC to update file
@@ -70,6 +73,9 @@ FEATURES CREATED BY INPUT FILE AND NEXT STEPS (where needed)
     is_callable
     is_puttable
     months_to_maturity_size
+
+ nodupe_trace_{ticker}_ltr.csv
+    recreate all the features above (excluding the identifiers), prefixed by '2_'
 
 for
  days in {1, 2, ..., 30}
@@ -84,6 +90,7 @@ from __future__ import division
 
 import argparse
 import collections
+import copy
 import cPickle as pickle
 import os
 import pandas as pd
@@ -106,8 +113,9 @@ import seven.feature_makers as feature_makers
 from seven.feature_makers import FeatureMakerEtf
 from seven.feature_makers import FeatureMakerFund
 from seven.feature_makers import FeatureMakerOhlc
+# from seven.feature_makers import FeatureMakerOtr
 from seven.feature_makers import FeatureMakerSecurityMaster
-from seven.feature_makers import FeatureMakerTicker
+from seven.feature_makers import FeatureMakerTrace
 from seven.feature_makers import FeatureMakerTradeId
 import seven.path
 
@@ -135,8 +143,8 @@ class Doit(object):
         self.in_fund = os.path.join(midpredictor, 'fundamentals', ticker + '_fund.csv')
         self.in_security_master = os.path.join(midpredictor, 'secmaster', ticker + '_and_comps_sec_master.csv')
         self.in_spx_equity_ohlc = os.path.join(midpredictor, 'tmp-todelete', 'spx_equity_ohlc.csv')
-        self.in_ticker = os.path.join(midpredictor, 'tmp-todelete', ticker + '.csv')
         self.in_ticker_equity_ohlc = os.path.join(midpredictor, 'tmp-todelete', ticker + '_equity_ohlc.csv')
+        self.in_trace = os.path.join(midpredictor, 'trace', 'nodupe_trace_%s_otr.csv' % ticker)
         self.out_cusips = [
             os.path.join(out_dir, self.make_outfile_name(cusip))
             for cusip in self.cusips
@@ -157,8 +165,8 @@ class Doit(object):
             self.in_fund,
             self.in_security_master,
             self.in_spx_equity_ohlc,
-            self.in_ticker,
             self.in_ticker_equity_ohlc,
+            self.in_trace,
         ]
 
     def __str__(self):
@@ -173,7 +181,7 @@ def make_control(argv):
     print argv
     parser = argparse.ArgumentParser()
     parser.add_argument('ticker')
-    parser.add_argument('--cusip', action='store', type=arg_type.cusip)
+    parser.add_argument('--cusips', action='store', nargs='*', type=arg_type.cusip)
     parser.add_argument('--test', action='store_true')
     parser.add_argument('--trace', action='store_true')
     arg = parser.parse_args(argv[1:])
@@ -203,21 +211,6 @@ def make_control(argv):
     )
 
 
-class Otr(object):
-    def __init__(self, df):
-        return  # for now
-        pdb.set_trace()
-        self.otr_cusip = {}
-        for index, record in df.iterrows():
-            original_index = record.originalsequencenumber
-            otr_cusip = record.bechmarkcusip
-            self.otr[original_index] = otr_cusip
-
-    def otr(self, ticker_index):
-        pdb.set_trace()
-        return self.otr[ticker_index]
-
-
 def read_csv(path, date_columns=None, usecols=None, index_col=0, nrows=None, parse_dates=None, verbose=True):
     if index_col is not None and usecols is not None:
         print 'cannot read both the index column and specific columns'
@@ -239,8 +232,14 @@ def read_csv(path, date_columns=None, usecols=None, index_col=0, nrows=None, par
 
 def do_work(control):
     'write order imbalance for each ticker_record in the input file'
-    def validate_ticker_records(df):
+    def validate_trace_records(df):
         assert (df.ticker == control.arg.ticker.upper()).all()
+        unique_ids = set()
+        for index, row in df.iterrows():
+            if index in unique_ids:
+                print 'error: found non-unique id/index', index
+                pdb.set_trace()
+            unique_ids.add(index)
 
     def read_equity_ohlc(path):
         return read_csv(
@@ -251,14 +250,15 @@ def do_work(control):
     # BODY STARTS HERE
 
     # read {ticker}.csv
-    df_ticker = read_csv(
-        control.doit.in_ticker,
-        index_col='issuepriceid',
+    trade_print_identifier_column_name = 'issuepriceid'
+    df_trace = read_csv(
+        control.doit.in_trace,
+        index_col=trade_print_identifier_column_name,
         nrows=1000 if control.arg.test else None,
-        parse_dates=['maturity', 'effectivedate', 'effectivetime'],
+        parse_dates=['maturity', 'effectivedate', 'effectivetime', ],
     )
-    validate_ticker_records(df_ticker)
-    df_ticker['effectivedatetime'] = feature_makers.make_effectivedatetime(df_ticker)
+    validate_trace_records(df_trace)
+    df_trace['effectivedatetime'] = feature_makers.make_effectivedatetime(df_trace)
 
     feature_maker_etf_agg = FeatureMakerEtf(
         df=read_csv(
@@ -292,7 +292,7 @@ def do_work(control):
             verbose=True,
         )
     )
-    feature_maker_ticker = FeatureMakerTicker(
+    feature_maker_trace = FeatureMakerTrace(
         order_imbalance4_hps={
             'lookback': 10,
             'typical_bid_offer': 2,
@@ -306,57 +306,95 @@ def do_work(control):
         feature_maker_id,
         feature_maker_ohlc,
         feature_maker_security_master,
-        feature_maker_ticker,
+        feature_maker_trace,
     )
 
-    def master_file_records(df_ticker):
-        for ticker_index, ticker_record in df_ticker.iterrows():
+    def master_file_records(df):
+        for ticker_index, ticker_record in df.iterrows():
             yield ticker_index, ticker_record
 
-    def selected(ticker_index, ticker_record):
-        if control.arg.cusip is not None:
-            if ticker_record.cusip == control.arg.cusip:
+    def selected(index, record):
+        'select records designated by control.arg.cusips, by returning (use_record, error_message)'
+        if control.arg.cusips is not None:
+            if record.cusip in control.arg.cusips:
                 return (True, None)
             else:
-                return (False, 'cusip was %s not the looked for %s' % (ticker_record.cusip, control.arg.cusip))
+                return (False, 'cusip was %s, not in the looked for %s' % (record.cusip, control.arg.cusips))
         else:
             return (True, None)
 
-    d = {}  # Dict[cusip, Dict[feature_name, feature_values]], maintained by append_features()
+    # pass 1: create features for the trades, create d:Dict
+    d = {}  # Dict[cusip, Dict[feature_name, feature_values:List[Number]]], maintained by append_features()
     create_features = timeseries.CreateFeatures()
     for features, ticker_index, ticker_record in create_features.create(
         feature_makers=all_feature_makers,
-        master_file_records=master_file_records(df_ticker),
+        master_file_records=master_file_records(df_trace),
         selected=selected,
-        verbose=False,
+        verbose=True,
     ):
         cusip = ticker_record.cusip
         if cusip not in d:
             d[cusip] = collections.defaultdict(list)
         for feature_name, feature_value in features.iteritems():
+            if feature_name == 'issuepriceid':
+                print 'found', feature_name
+                pdb.set_trace()
             d[cusip][feature_name].append(feature_value)
         # otr[ticker_index] = get_otr_cusip(ticker_index)
-    print 'read %d ticker records across %d cusips' % (create_features.n_input_records, len(d))
-    print 'wrote %d feature records' % create_features.n_output_records
-    print 'writing output files'
+
+    # pass 2: append features from the on-the-run bonds, create dd:Dict
+    def is_otr_feature_name(feature_name):
+        return not feature_name.startswith('id_')
+
+    def otr_feature_name(off_the_run_feature_name):
+        return 'otr1_' + off_the_run_feature_name
+
+    def extend_feature_columns(existing_feature_name_values, new_feature_name_values, feature_name_prefix):
+        'return new features dict incorporating both existing and new names and values'
+        result = {}
+        for existing_name, existing_values in existing_feature_name_values.iteritems():
+            if existing_name.startswith('p_'):
+                result[existing_name] = existing_values
+                result[feature_name_prefix + existing_name[2:]] = new_feature_name_values[feature_name]
+            elif existing_name.startswith('id_'):
+                result[existing_name] = existing_values
+            else:
+                print 'error: unexpected feature_name', existing_name
+                pdb.set_trace()
+        return result
+
+    # NOTE: it's possible that the on-the-run and off-the-run CUSIPs are identical, in which case,
+    # we have duplicate feature values. That isn't a problem for the decision tree-based methods,
+    # which will randomly choose one of the duplicate columns
+    dd = copy.deepcopy(d)
+    # add new columns to each cusip's dictionary
     for cusip in d.keys():
+        cusip1 = feature_maker_trace.otr1[cusip]
+        dd[cusip] = extend_feature_columns(dd[cusip], dd[cusip1], 'otr1_')
+
+    # report and write output file
+    print 'read %d ticker records across %d cusips' % (create_features.n_input_records, len(dd))
+    print 'created %d feature records across all cusips' % create_features.n_output_records
+    print 'writing output files'
+    for cusip in dd.keys():
         # check that length of values is the same
         print 'feature names, # records (should be same for all)'
         common_length = None
-        for feature_name in sorted(d[cusip].keys()):
-            v = d[cusip][feature_name]
+        for feature_name in sorted(dd[cusip].keys()):
+            v = dd[cusip][feature_name]
             print ' %45s %6d' % (feature_name, len(v))
             if common_length is None:
                 common_length = len(v)
             else:
                 assert len(v) == common_length
         df = pd.DataFrame(
-            data=d[cusip],
-            index=d[cusip]['id_ticker_index'],
+            data=dd[cusip],
+            index=dd[cusip]['id_ticker_index'],
         )
         filename = control.doit.make_outfile_name(cusip)
         path = os.path.join(control.doit.out_dir, filename)
-        df.to_csv(path)
+        df_sorted = df.sort_index(axis=1)
+        df_sorted.to_csv(path)
         print 'wrote %d records to %s' % (len(df), filename)
     print 'reasons records were skipped'
     for msg, count in create_features.skipped.iteritems():
