5e0df4953f551e72f8cd5286f916d0184e0b46fd
Roy Lowrance <roy.lowrance@gmail.com>
2017-05-14 10:20:01 -0400

    add analysis of importances (wip)

--- report_compare_models2.py
+++ report_compare_models2.py
@@ -11,6 +11,7 @@ from __future__ import division
 
 import argparse
 import collections
+import heapq
 import os
 import pdb
 import pandas as pd
@@ -60,14 +61,22 @@ def make_control(argv):
     )
 
 
-Data = collections.namedtuple(
-    'Data',
+Error = collections.namedtuple(
+    'Error',
     'query_index model_spec_str target_feature_name actual predicted absolute_error',
+)
+Importance = collections.namedtuple(
+    'Importance',
+    'query_index model_spec_str target_feature_name prediction_feature_name importance'
 )
 
 
 def column_def(column_name):
     return seven.reports.all_columns_2[column_name]
+
+
+def column_defs(*column_names):
+    return map(column_def, column_defs)
 
 
 def write_the_lines(heading_lines, table_lines, path, verbose=True):
@@ -80,15 +89,94 @@ def write_the_lines(heading_lines, table_lines, path, verbose=True):
         f.writelines('%s\n' % line for line in heading_lines + table_lines)
 
 
-def reports_mean_errors(data, path, arg):
-    'write several reports on the mean errors'
+def reports_best_features(best_models, importances):
+    def make_best():
+        'summarize best models for key targetfeaturename and modelspecstr'
+        heaps = collections.defaultdict(list)
+        for targetfeaturename, d in best_models.iteritems():
+            for modelspecstr, meanerror in d.iteritems():
+                modelname = modelspecstr.split('-')[0]
+                if modelname != 'n':  # skip naive models
+                    heapq.heappush(heaps[(modelname, targetfeaturename)], (meanerror, modelspecstr))
+        # for now, determine the best models for each modelname
+        best = collections.defaultdict(lambda: collections.defaultdict(list))
+        worst = collections.defaultdict(lambda: collections.defaultdict(list))
+        for modelname_targetfeaturename, a_list in heaps.iteritems():
+            for n in (1, 10):
+                best[modelname_targetfeaturename][n] = heapq.nsmallest(n, a_list)
+                worst[modelname_targetfeaturename][n] = heapq.nlargest(n, a_list)
+        print 'best N models by modelname'
+        for modelname_targetfeaturename, d in best.iteritems():
+            for n, a_list in d.iteritems():
+                print modelname_targetfeaturename, n
+                for i, item in enumerate(a_list):
+                    print ' ', i, item
+        print 'worst N models by modelname'
+        for modelname_targetfeaturename, d in worst.iteritems():
+            for n, a_list in d.iteritems():
+                print modelname_targetfeaturename, n
+                for i, item in enumerate(a_list):
+                    print ' ', i, item
+        return best
+
+    def summarize_importances(importances):
+        'return (Dict[(modelspecstr, targetfeaturename, predictedfeaturename), float], predictedfeaturenames)'
+        importances_by = collections.defaultdict(dict)
+        predicted_feature_names = set()
+        for i in importances:
+            if i.importance != 0.0:
+                # if i.model_spec_str == 'rf-1-----10--' and i.target_feature_name.endswidth('S'):
+                #     print i.model_spec_str, i.target_feature_name, i.prediction_feature_name, i.importance
+                key1 = (i.model_spec_str, i.target_feature_name, i.prediction_feature_name)
+                importances_by[key1][i.query_index] = i.importance
+                predicted_feature_names.add(i.prediction_feature_name)
+        return importances_by, predicted_feature_names
+
+    def report_on_best_model():
+        best = make_best()
+        importances_by, predictedfeaturenames = summarize_importances(importances)
+        headings = [
+            'Importances of the best models',
+            'Excluding features with zero importance',
+            'By model name and target feature'
+            ' '
+        ]
+        pp(headings)
+        # product the detail lines for the best model for each (modelname, targetfeature)
+        details = []
+        pdb.set_trace()
+        for (modelname, targetfeaturename), d in best.iteritems():
+            for n, meanerrors_modelspecstrs in d.iteritems():
+                if n == 1:
+                    pdb.set_trace()
+                    meanerror, modelspecstr = meanerrors_modelspecstrs[0]
+                    print targetfeaturename, modelspecstr
+                    for predictionfeaturename in predictedfeaturenames:
+                        i = importances_by[(modelspecstr, targetfeaturename, predictionfeaturename)]
+                        for queryindex, importance in i.iteritems():
+                            pdb.set_trace()
+                            details.append(queryindex, modelspecstr, targetfeaturename, predictionfeaturename, importance)
+        pdb.set_trace()
+        ct = columns_table(
+            column_defs('query_index', 'model_spec', 'target_feature', 'prediction_feature', 'importance'),
+            details,
+        )
+        pp(ct)
+        pdb.set_trace()
+
+    summary = report_on_best_model()
+    print summary
+
+
+def reports_mean_errors(errors, path, arg):
+    'write several reports on the mean errors; return dictionary of mean errors by target and modelspec'
     def sort_by_value(d):
         return collections.OrderedDict(sorted(d.items(), key=lambda x: x[1]))
 
     def sort_by_key(d):
         return collections.OrderedDict(sorted(d.items(), key=lambda x: x[0]))
 
-    def make_summary(data):
+    def make_summary(errors):
         'return dict of summaries of the data'
         counter_modelspecstr = collections.Counter()
         counter_queryindex = collections.Counter()
@@ -102,7 +190,7 @@ def reports_mean_errors(data, path, arg):
         data_by_targetfeaturename_modelspecstr = collections.defaultdict(lambda: collections.defaultdict(list))
         data_by_queryindex_targetfeaturename = collections.defaultdict(lambda: collections.defaultdict(list))
 
-        for x in data:
+        for x in errors:
             # counters
             counter_modelspecstr[x.model_spec_str] += 1
             counter_queryindex[x.query_index] += 1
@@ -145,7 +233,7 @@ def reports_mean_errors(data, path, arg):
             'means_by_queryindex_targetfeaturename': make_means2(data_by_queryindex_targetfeaturename),
         }
 
-    summary = make_summary(data)
+    summary = make_summary(errors)
 
     headings = [
         'Mean Absolute Errors',
@@ -217,11 +305,56 @@ def reports_mean_errors(data, path, arg):
         'out_accuracy_queryindex_targetfeaturename',
     )
 
+    # return dictionary of mean absolute errors by target_feature and model_spec
+    # sorted in ascending order of mean absolute error
+    return summary['means_by_targetfeaturename_modelspecstr']
 
-def read_and_transform_input(control):
-    'return [Data]'
+
+def read_and_transform_importances(control):
+    'return [Error]'
     data = []
-    for in_file_path in control.path['in_files']:
+    for in_file_path in control.path['in_importances']:
+        if os.path.getsize(in_file_path) == 0:
+            print 'skipping empty file: %s' % in_file_path
+            continue
+        importances = pd.read_csv(
+            in_file_path,
+            index_col=[0, 1, 2, 3],
+        )
+        print 'read %d importances from file %s' % (len(importances), in_file_path.split('\\')[-2])
+        nonzero_importances = importances.loc[importances.importance != 0.0]
+        print 'retained the %d that had non-zero importances' % len(nonzero_importances)
+        n_has_nan = 0
+        for row_index, row_data in nonzero_importances.iterrows():
+            has_nan = False
+            # exclude records with any NaN values
+            for series_index, series_value in row_data.iteritems():
+                if series_value != series_value:
+                    has_nan = True
+                    break
+            if has_nan:
+                n_has_nan += 1
+            else:
+                data.append(Importance(
+                    prediction_feature_name=row_data['id_feature_name'],
+                    model_spec_str=row_data['id_modelspec_str'],
+                    query_index=row_data['id_query_index'],
+                    target_feature_name=row_data['id_target_feature_name'],
+                    importance=row_data['importance'],
+                ))
+        if control.arg.test:
+            print 'stopping reading, because testing'
+            break
+    print
+    print 'read %d records from %d input files' % (len(data), len(control.path['in_importances']))
+    print 'skipped %d records because at least one field was missing (NaN)' % n_has_nan
+    return data
+
+
+def read_and_transform_predictions(control):
+    'return [Error]'
+    data = []
+    for in_file_path in control.path['in_predictions']:
         if os.path.getsize(in_file_path) == 0:
             print 'skipping empty file: %s' % in_file_path
             continue
@@ -241,7 +374,7 @@ def read_and_transform_input(control):
             if has_nan:
                 n_has_nan += 1
             else:
-                data.append(Data(
+                data.append(Error(
                     query_index=row_data['id_query_index'],
                     model_spec_str=row_data['id_modelspec_str'],
                     target_feature_name=row_data['id_target_feature_name'],
@@ -253,15 +386,15 @@ def read_and_transform_input(control):
             print 'stopping reading, because testing'
             break
     print
-    print 'read %d records from %d input files' % (len(data), len(control.path['in_files']))
+    print 'read %d records from %d input files' % (len(data), len(control.path['in_predictions']))
     print 'skipped %d records because at least one field was missing (NaN)' % n_has_nan
     return data
 
 
-def check_data(data):
+def check_errors(errors):
     'stop if a problem with the input data'
     index_counts = collections.Counter()
-    for x in data:
+    for x in errors:
         index_counts[x.query_index] += 1
 
     print 'counts by query_index'
@@ -279,15 +412,18 @@ def check_data(data):
 
 def do_work(control):
     'produce reports'
-    data = read_and_transform_input(control)
-    check_data(data)
-
-    # produce each report or set of reports
-    reports_mean_errors(
-        data,
+    # produce reports on mean absolute errors
+    errors = read_and_transform_predictions(control)
+    check_errors(errors)
+    best_models = reports_mean_errors(
+        errors,
         control.path,
         control.arg,
     )
+
+    # produce reports on importancers of features for the most accurate models
+    importances = read_and_transform_importances(control)
+    reports_best_features(best_models, importances)
 
 
 def main(argv):
