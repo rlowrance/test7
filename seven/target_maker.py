d58578e8878a1981d38c8f6982c32a15f9397aac
Roy Lowrance <roy.lowrance@gmail.com>
2017-05-26 07:46:56 -0400

    rename file

--- seven/target_maker.py
+++ seven/target_maker.py
@@ -1,224 +1,58 @@
-'''make targets from features
-
-Example 3:
- Given these features
-        oas    trade
- time   spread type quantity
- 10:00     100 B    100
- 10:01     101 D    200
- 10:02     102 S    300
-
- Expect these targets
-        next next next
- time    B   D   S
- 10:00 nan 101 102
- 10:01 nan nan 102
-
-Example 4:
- Given these features
-        oas    trade
- time   spread type quantity
- 10:00     100 B    100
- 10:01     101 D    200
- 10:02     102 S    300
- 10:03     103 B    400
- 10:03     105 B    400
- 10:03     105 D    600
- 10:03     106 S    700
-
- Expect these targets
-        next next next
- time    B   D   S
- 10:00 104 101 102
- 10:01 104 105 102
- 10:02 104 105 106
+'''make targets from trace prints
 '''
 import collections
-import datetime
 import numpy as np
 import pandas as pd
 import pdb
-import unittest
 
 
-def make_training_targets(features, verbose=False, trace=False):
-    'return DataFrame with next oasspread weighted-average prices'
-    # NOTE: the index of ther return DataFrame cannot be the same as the features dataframe
-    # because the returned DataFrame has one row for each effectivedatetime and the features dataframe
-    # may have multiple rows on the same effectivedatetime
+class TargetMaker(object):
+    def __init__(self):
+        self.data = collections.defaultdict(list)  # Dict[target_name, target_value]
+        self.indices = []
 
-    def isnan(x):
-        return x != x
+    def append_targets(self, trace_index, trace_record):
+        'append info to self.data and self.indices; return None or err'
+        def append_to_data(targets_values):
+            for target, value in targets_values.iteritems():
+                self.data[target]. append(value)
+            return None
 
-    def make_weighted_average_features(features):
-        'return Dict[effectivedatetime, Dict[trade_type, weighted_average_oasspread]]'
-        result = collections.defaultdict(dict)
-        for effectivedatetime in set(features['id_effectivedatetime']):
-            at_effectivedatetime = features.loc[features['id_effectivedatetime'] == effectivedatetime]
-            for trade_type in ('B', 'D', 'S'):
-                for_trade_type_mask = at_effectivedatetime['p_trade_type_is_%s' % trade_type] == 1
-                for_trade_type = at_effectivedatetime.loc[for_trade_type_mask]
-                if len(for_trade_type) == 0:
-                    result[effectivedatetime][trade_type] = np.nan
-                else:
-                    weighted_sum = (for_trade_type['p_quantity_size'] * for_trade_type['p_oasspread']).sum()
-                    weighted_average = weighted_sum / for_trade_type['p_quantity_size'].sum()
-                    result[effectivedatetime][trade_type] = weighted_average
-        return result
+        def append_to_indices(trace_index):
+            self.indices.append(trace_index)
 
-    if verbose:
-        print 'make_training_targets: features'
-        print features
-    if trace:
-        pdb.set_trace()
-    weighted_average_features = make_weighted_average_features(features)
-    effectivedatetimes = sorted(weighted_average_features.keys())
-    d = collections.defaultdict(list)
-    for i, effectivedatetime in enumerate(effectivedatetimes[:-1]):  # don't look at the last time
-        # search forwad to find the next trade for each trade_type
-        next_wa_oasspread = {}
-        next_wa_effectivedatetime = {}
-        for trade_type in ('B', 'D', 'S'):
-            # search forward until we maybe find the next oasspread for the trade_type
-            for k in xrange(i + 1, len(effectivedatetimes), 1):
-                candidate_effectivedatetime = effectivedatetimes[k]
-                wa = weighted_average_features[candidate_effectivedatetime][trade_type]
-                if not isnan(wa):
-                    next_wa_oasspread[trade_type] = wa
-                    next_wa_effectivedatetime[trade_type] = candidate_effectivedatetime
-                    break
-        d['id_effectivedatetime'].append(effectivedatetime)
-        d['target_next_oasspread_B'].append(next_wa_oasspread.get('B', np.nan))
-        d['target_next_oasspread_D'].append(next_wa_oasspread.get('D', np.nan))
-        d['target_next_oasspread_S'].append(next_wa_oasspread.get('S', np.nan))
-        d['info_target_next_effectivedatetime_B'].append(next_wa_effectivedatetime.get('B', np.nan))
-        d['info_target_next_effectivedatetime_D'].append(next_wa_effectivedatetime.get('D', np.nan))
-        d['info_target_next_effectivedatetime_S'].append(next_wa_effectivedatetime.get('S', np.nan))
-    if trace:
-        pdb.set_trace()
-    if len(d) == 0:
-        # we created no records
+        targets_values, err = self._make_targets_values(trace_index, trace_record)
+        if err is not None:
+            return err
+        append_to_data(targets_values)
+        append_to_indices(trace_index)
+        return None
+
+    def get_targets_dataframe(self):
+        'return DataFrame'
         result = pd.DataFrame(
-            columns=[
-                'id_effectivedatetime',
-                'target_next_oasspread_B',
-                'target_next_oasspread_D',
-                'target_next_oasspread_S',
-                'info_target_next_effectivedatetime_B',
-                'info_target_next_effectivedatetime_D',
-                'info_target_next_effectivedatetime_S',
-            ],
+            data=self.data,
+            index=self.indices,
         )
         return result
-    else:
-        result = pd.DataFrame(data=d)
-        if verbose:
-            print 'make_training_targets: result'
-        return result
 
+    def _make_targets_values(self, trace_index, trace_record):
+        'return (dict, err)'
+        oasspread = trace_record['oasspread']
+        if np.isnan(oasspread):
+            return None, 'oasspread is NaN'
+        trade_type = trace_record['trade_type']
+        assert trade_type in ('B', 'D', 'S')
+        result = {
+            'id_trace_index': trace_index,
+            'id_trade_type': trade_type,
+            'id_oasspread': oasspread,
+            'id_effectivedatetime': trace_record['effectivedatetime'],
+            'target_oasspread_B': oasspread if trade_type == 'B' else np.nan,
+            'target_oasspread_D': oasspread if trade_type == 'D' else np.nan,
+            'target_oasspread_S': oasspread if trade_type == 'S' else np.nan,
+        }
+        return result, None
 
-class TestAllTargets(unittest.TestCase):
-    def test_multiple_trades_at_same_time(self):
-        def isnan(x):
-            return x != x
-
-        def make_effectivedatetime(time):
-            'convert hours.minutes to datetime.datetime'
-            hours = int(time)
-            minutes = int(round(100 * (time - hours)))
-            return datetime.datetime(2016, 11, 1, hours, minutes)
-
-        def assert_equal(expected, actual):
-            # print 'assert_equal:', expected, actual
-            self.assertEqual(len(expected), len(actual))
-            for i in xrange(len(expected)):
-                # print expected[i], actual[i]
-                if isnan(expected[i]):
-                    self.assertTrue(isnan(actual[i]))
-                else:
-                    self.assertEqual(expected[i], actual[i])
-
-        def make_expected(rows):
-            'return DataFrame'
-            result = pd.DataFrame(
-                columns=[
-                    'id_effectivedatetime',
-                    'target_next_oasspread_B',
-                    'target_next_oasspread_D',
-                    'target_next_oasspread_S',
-                ],
-            )
-            for row in rows:
-                test_time, b, d, s = row
-                result.loc[len(result)] = (make_effectivedatetime(test_time), b, d, s)
-            return result
-
-        def make_features(highest_feature_time, features_data):
-            'return DataFrame'
-            result = pd.DataFrame(
-                columns=[
-                    'id_effectivedatetime',
-                    'p_oasspread',
-                    'p_trade_type_is_B',
-                    'p_trade_type_is_D',
-                    'p_trade_type_is_S',
-                    'p_quantity_size',
-                ],
-            )
-            for trace_time, trace_prints in features_data:
-                for trace_print in trace_prints:
-                    oasspread, trade_type, quantity = trace_print
-                    result.loc[len(result)] = (
-                        make_effectivedatetime(trace_time),
-                        oasspread,
-                        1 if trade_type == 'B' else 0,
-                        1 if trade_type == 'D' else 0,
-                        1 if trade_type == 'S' else 0,
-                        quantity,
-                    )
-                if trace_time == highest_feature_time:
-                    break
-            return result
-
-        verbose = False
-        features_data = (
-            (10.00, [(100, 'B', 100)]),
-            (10.01, [(101, 'D', 200)]),
-            (10.02, [(102, 'S', 300)]),
-            (10.03, [(103, 'B', 400), (105, 'B', 400), (105, 'D', 600), (106, 'S', 700)]),
-            (10.04, [(107, 'B', 800)]),
-        )
-        nan = np.nan
-        tests = (  # (highest feature time, [rows])
-            (10.00, []),
-            (10.01, [(10.00, nan, 101, nan)]),
-            (10.02, [(10.00, nan, 101, 102),
-                     (10.01, nan, nan, 102)]),
-            (10.03, [(10.00, 104, 101, 102),
-                     (10.01, 104, 105, 102),
-                     (10.02, 104, 105, 106)]),
-            (10.04, [(10.00, 104, 101, 102),
-                     (10.01, 104, 105, 102),
-                     (10.02, 104, 105, 106),
-                     (10.03, 107, nan, nan)]),
-        )
-        for test in tests:
-            highest_feature_time, rows = test
-            expected = make_expected(rows)
-            features = make_features(highest_feature_time, features_data)
-            actual = make_training_targets(features)
-            if verbose:
-                print 'actual'
-                print actual
-                print 'expected'
-                print expected
-                pdb.set_trace()
-            for column_name in expected.columns:
-                # NOTE: ignore the info columns in this test
-                assert_equal(expected[column_name], actual[column_name])
-
-
-if __name__ == '__main__':
-    unittest.main()
-    if False:
-        pdb
+if False:
+    pdb
