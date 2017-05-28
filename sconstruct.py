21d9a7ad7752e7ce68d505eba0b4a078bae83b45
Roy Lowrance <roy.lowrance@gmail.com>
2017-05-28 12:25:03 -0400

    all days in Nov

--- sconstruct.py
+++ sconstruct.py
@@ -2,6 +2,7 @@
 #   scons -f sconstruct.py /
 #   scons -n -f sconstruct.py /
 #   scons --debug=explain -f sconstruct.py /
+#   scons -j <nprocesses> -f sconstruct.py /
 
 # where / means to build everything (not just stuff in the current working directory .)
 import os
@@ -36,18 +37,16 @@ def command(*args):
 
 
 # main program
-tickers = ['orcl']
+tickers = ['ORCL']
 # all dates in November 2016
 # weekends: 5, 6, 12, 13, 19, 20, 26, 27
 # thanksgiving: 24
 dates = [ 
-    '%d-%02d-%02d' % (2016, 11, day)
-    for day in range(2, 30)
+    '%d-%02d-%02d' % (2016, 11, day + 1)
+    for day in xrange(30)  # 30 days in November
 ]
 for ticker in tickers:
     command(seven.build.cusips, ticker)
-    # command(seven.build.features, ticker)
-    # command(seven.build.targets, ticker)
     for cusip in ['68389XAS4']:  # just one cusip, for now
         for hpset in ['grid3']:
             for effective_date in dates:
