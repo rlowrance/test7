61508ff034368ea9384a7de7d4cf8bb0a9168234
Roy Lowrance <roy.lowrance@gmail.com>
2017-07-02 09:26:57 -0400

    fit using all trade types

--- seven/models.py
+++ seven/models.py
@@ -46,6 +46,11 @@ class Model(timeseries.Model):
         # names of columns on which we rely
         self.column_effectivedatetime = 'id_effectivedatetime'
 
+        # for now, allow only one target. Later allow others (such as inter-trade time)
+        if self.target_name != 'target_oasspread':
+            print 'unexpected target_name', self.target_name
+            pdb.set_trace()
+
     # helper methods for subclasses to use
     def _sorting_indices(self, df):
         'return indices that reorder the df by self.column_effectivedatetime'
@@ -64,33 +69,68 @@ class Model(timeseries.Model):
         'common fitting procedure for scikit-learn models'
         # this code implements the decision in file targets2.txt
 
-        # FIXME: In account for n_trades_back, we view the sorted trade print streams as ordered elswhere
+        # MAYBE: In account for n_trades_back, we view the sorted trade print streams as ordered elswhere
         # however, we know the time stamps for each trace print.
         # An alternative approach is to use weighted average feature values for each time period.
         # We would apply this alternative approach to trades at the edge of the lookback window
         if trace:
             pdb.set_trace()
+
+        # remove trades that are not for the self.target_name
+        if False:
+            # the original version of the fitting code fitted only on the transactions of the same trade type
+            # as the target
+            # It's better to train on as much data as possible
+            mask = np.isnan(training_targets[self.target_name])
+            relevant_targets = training_targets.loc[~mask]
+            relevant_features = training_features.loc[~mask]
+            if len(relevant_targets) == 0:
+                raise ExceptionFit('no training data after remove non-target samples')
+        else:
+            relevant_features = training_features
+            relevant_targets = training_targets
+
+        # select the number of trades back
+        # that requires sorting the training data
+        pdb.set_trace()
+        sorted_features = relevant_features.sort_values('id_effectivedatetime')
+        sorted_targets = relevant_targets.loc[sorted_features.index]
         n_trades_back = self.model_spec.n_trades_back
-        in_window_features = training_features.iloc[-n_trades_back:]
-        in_window_targets = training_targets.iloc[-n_trades_back:]
-        if len(in_window_features) == 0:
-            raise ExceptionFit('no training data within lookback window')
-        # Drop rows that dont' have the predicted variable in their targets
-        bad_indices = []
-        assert (in_window_features.index == in_window_targets.index).all()
-        for index, row in in_window_targets.iterrows():
-            if np.isnan(row[self.target_name]):
-                bad_indices.append(index)
-        relevant_training_features = in_window_features.drop(bad_indices)
-        relevant_training_targets = in_window_targets.drop(bad_indices)
+        relevant_training_features = sorted_features.iloc[-n_trades_back:]
+        relevant_training_targets = sorted_targets.iloc[-n_trades_back:]
         if len(relevant_training_features) == 0:
-            raise ExceptionFit('no training data with %s' % self.target_name)
+            raise ExceptionFit('no training data after looking back n trades (%d)' % n_trades_back)
+
+        # # only use the n_trade_back remaining trades
+
+        # n_trades_back = self.model_spec.n_trades_back
+        # in_window_features = sorted_features.iloc[-n_trades_back:]
+        # in_window_targets = sorted_targets.iloc[-n_trades_back:]
+        # if len(in_window_features) == 0:
+        #     raise ExceptionFit('no training data within lookback window')
+
+        # # Drop rows that dont' have the predicted variable in their targets
+        # # Train only samples that are for the requested target variable (which is in self.target_name)
+        # # # NOTE: this selection interacts with the n_trades_back model spec to use often fewer trades
+        # # than n_trades_back specifies
+        # bad_indices = []
+        # assert (in_window_features.index == in_window_targets.index).all()
+        # for index, row in in_window_targets.iterrows():
+        #     if np.isnan(row[self.target_name]):
+        #         bad_indices.append(index)
+        # relevant_training_features = in_window_features.drop(bad_indices)
+        # relevant_training_targets = in_window_targets.drop(bad_indices)
+        # if len(relevant_training_features) == 0:
+        #     raise ExceptionFit('no training data with %s' % self.target_name)
+
         feature_names, x = self._make_featurenames_x(relevant_training_features)
         self.feature_names = feature_names
         y = self._make_y(relevant_training_targets)
         try:
             self.model.fit(x, y)
         except Exception as e:
+            print 'exception in _fit:', e
+            pdb.set_trace()
             raise ExceptionFit(e)
 
     def _predict(self, query_features, trace=False):
@@ -108,7 +148,7 @@ class Model(timeseries.Model):
             return vector
         elif transform == 'log':
             return np.log(vector)
-        elif transform is 'log1p':
+        elif transform == 'log1p':
             return np.log1p(vector)
         else:
             print 'error: unexpected transform: %s' % transform
@@ -167,7 +207,11 @@ class Model(timeseries.Model):
             # print raw_column
             # print transformed_column
             # print self.target_name
-            raise ExceptionFit('_make_y: NaN in transformed column %s: raw %s' % (self.target_name, raw_column))
+            pdb.set_trace()
+            raise ExceptionFit('_make_y: NaN in transformed column %s transformation %s' % (
+                self.target_name,
+                self.model_spec.transform_y,
+            ))
         if trace:
             pdb.set_trace()
         return transformed_column
@@ -182,11 +226,9 @@ class ModelNaive(Model):
         'predict the last trade of the type '
         'simple save the last value of the feature we want to predict'
         assert len(training_targets) > 0
-        self.prediction = training_targets.iloc[-1][self.target_name]
-        # last_feature = training_features.iloc[-1]  # the traiing_features are sorted by increasing effectivedatetime
-        # feature_name_used = 'p_oasspread'
-        # self.model = last_feature['p_oasspread']  # predict the spread of the most recent training features
-        self.importances = {self.target_name: 1.0}
+        sorted_training_targets = training_targets.sort_values('id_effectivedatetime')
+        self.prediction = sorted_training_targets.iloc[-1][self.target_name]
+        self.importances = None
 
     def predict(self, query_features):
         'predict the most recent historic trade for the trade_type'
@@ -245,7 +287,7 @@ class ModelRandomForests(Model):
         )
 
     def fit(self, training_features, training_targets):
-        self._fit(training_features, training_targets, trace=False)
+        self._fit(training_features, training_targets)
         self.importances = {}
         for i, importance in enumerate(self.model.feature_importances_):
             self.importances[self.feature_names[i]] = importance
