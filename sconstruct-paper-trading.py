16f9584af131580868797333af8a77d0eeb061a8
Roy Lowrance <roy.lowrance@gmail.com>
2017-07-02 17:02:07 -0400

    rules for fit.py

--- sconstruct-paper-trading.py
+++ sconstruct-paper-trading.py
@@ -19,6 +19,8 @@ import pdb
 import pprint
 
 import seven.build
+import seven.GetBuildInfo
+
 pp = pprint.pprint
 pdb
 
@@ -45,89 +47,114 @@ def command(*args):
     )
 
 
-dt = datetime.date
+# dt = datetime.date
 
-Info = collections.namedtuple('Info', 'issuer first_date last_date')
-
-
-cusip_infos = {
-    '037833AG5': Info('AAPL', dt(2017, 5, 1), dt(2017, 6, 26)),
-    # '037833AG5': Info('AAPL', dt(2017, 6, 1), dt(2017, 6, 1)),
-    # '68389XAS4': Info('ORCL', dt(2016, 11, 1), dt(2016, 11, 2), "test"),  # test this scons file
-    # '68389XAC9': Info('ORCL', dt(2016, 9, 1), dt(2016, 12, 31), "dell"),   # requests from Katrina in email
-    # '68389XBJ3': Info('ORCL', dt(2016, 6, 29), dt(2017, 3, 16), "alien"),
-    # '68389XBC8': Info('ORCL', dt(2015, 4, 28), dt(2017, 3, 16), "mac"),
-    # '68389XBM6': Info('ORCL', dt(2016, 6, 29), dt(2017, 3, 16), "dell"),
-    # '68389XAG0': Info('ORCL', dt(2012, 1, 4), dt(2017, 3, 16), "done"),
-    # '68389XAK1': Info('ORCL', dt(2012, 1, 3), dt(2017, 3, 16), "mac"),
-    # '68389XAM7': Info('ORCL', dt(2012, 1, 3), dt(2017, 3, 16), "mac"),
-    # '68389XBA2': Info('ORCL', dt(2014, 6, 30), dt(2017, 3, 16), "dell"),
-    # '68389XBD6': Info('ORCL', dt(2015, 4, 28), dt(2017, 3, 16), "alien"),
-    # '68389XBE4': Info('ORCL', dt(2015, 4, 28), dt(2017, 3, 16), "alien"),
-    # '68389XBK0': Info('ORCL', dt(2016, 6, 29), dt(2017, 3, 16), "dell"),
-}
+# Info = collections.namedtuple('Info', 'issuer first_date last_date')
 
 
-def select_cusip_from_info_where_run_id_equals(run_id):
-    'return set of cusips'
-    result = {
-        cusip
-        for cusip, info in info.iteritems()
-        if info.run_id == run_id
-    }
-    return result
+# cusip_infos = {
+#     '037833AG5': Info('AAPL', dt(2017, 5, 1), dt(2017, 6, 26)),
+#     # '037833AG5': Info('AAPL', dt(2017, 6, 1), dt(2017, 6, 1)),
+#     # '68389XAS4': Info('ORCL', dt(2016, 11, 1), dt(2016, 11, 2), "test"),  # test this scons file
+#     # '68389XAC9': Info('ORCL', dt(2016, 9, 1), dt(2016, 12, 31), "dell"),   # requests from Katrina in email
+#     # '68389XBJ3': Info('ORCL', dt(2016, 6, 29), dt(2017, 3, 16), "alien"),
+#     # '68389XBC8': Info('ORCL', dt(2015, 4, 28), dt(2017, 3, 16), "mac"),
+#     # '68389XBM6': Info('ORCL', dt(2016, 6, 29), dt(2017, 3, 16), "dell"),
+#     # '68389XAG0': Info('ORCL', dt(2012, 1, 4), dt(2017, 3, 16), "done"),
+#     # '68389XAK1': Info('ORCL', dt(2012, 1, 3), dt(2017, 3, 16), "mac"),
+#     # '68389XAM7': Info('ORCL', dt(2012, 1, 3), dt(2017, 3, 16), "mac"),
+#     # '68389XBA2': Info('ORCL', dt(2014, 6, 30), dt(2017, 3, 16), "dell"),
+#     # '68389XBD6': Info('ORCL', dt(2015, 4, 28), dt(2017, 3, 16), "alien"),
+#     # '68389XBE4': Info('ORCL', dt(2015, 4, 28), dt(2017, 3, 16), "alien"),
+#     # '68389XBK0': Info('ORCL', dt(2016, 6, 29), dt(2017, 3, 16), "dell"),
+# }
 
 
-def select_ticker_from_info_where_run_id_equals(run_id):
-    'return set of tickers'
-    result = {
-        info.ticker
-        for cusip, info in info.iteritems()
-        if info.run_id == run_id
-    }
-    return result
+# def select_cusip_from_info_where_run_id_equals(run_id):
+#     'return set of cusips'
+#     result = {
+#         cusip
+#         for cusip, info in info.iteritems()
+#         if info.run_id == run_id
+#     }
+#     return result
 
 
-def select_ticker_from_info_where_cusip_equals(selected_cusip):
-    'return set of tickers'
-    result = {
-        info.ticker
-        for cusip, info in info.iteritems()
-        if cusip == selected_cusip
-    }
-    return result
+# def select_ticker_from_info_where_run_id_equals(run_id):
+#     'return set of tickers'
+#     result = {
+#         info.ticker
+#         for cusip, info in info.iteritems()
+#         if info.run_id == run_id
+#     }
+#     return result
 
 
-def to_str(dt):
-    'convert datetime.date to str'
-    result = '%4d-%02d-%02d' % (dt.year, dt.month, dt.day)
-    return result
+# def select_ticker_from_info_where_cusip_equals(selected_cusip):
+#     'return set of tickers'
+#     result = {
+#         info.ticker
+#         for cusip, info in info.iteritems()
+#         if cusip == selected_cusip
+#     }
+#     return result
 
 
-def effective_dates_for_cusip(cusip, info):
-    'return list of dates (:str) to be fed to features_targets'
-    next_date = info.first_date
-    last_date = info.last_date
-    result = []
-    while next_date <= last_date:
-        result.append(to_str(next_date))
-        next_date += datetime.timedelta(days=1)
-    return result
+# def to_str(dt):
+#     'convert datetime.date to str'
+#     result = '%4d-%02d-%02d' % (dt.year, dt.month, dt.day)
+#     return result
 
 
-def hpset_for(cusip):
-    return 'grid3'
+# def effective_dates_for_cusip(cusip, info):
+#     'return list of dates (:str) to be fed to features_targets'
+#     next_date = info.first_date
+#     last_date = info.last_date
+#     result = []
+#     while next_date <= last_date:
+#         result.append(to_str(next_date))
+#         next_date += datetime.timedelta(days=1)
+#     return result
+
+
+# def hpset_for(cusip):
+#     return 'grid3'
 
 
 def commands_for():
     'call command(*args) for appropriate targets'
     # for now, just work with one CUSIP and hence one issuer
+    issuers = ('AAPL',)
+    issuer_cusips = {
+        'AAPL': ('037833AG5',),
+    }
+    fit_dates = ('2017-06-26',)
+    hpset = 'grid4'
 
+    # buildinfo.py
     for issuer in ('AAPL',):
         command(seven.build.buildinfo, issuer)
-    for cusip, info in cusip_infos.iteritems():
-        for effective_date in effective_dates_for_cusip(cusip, info):
-            command(seven.build.features_targets, info.issuer, cusip, effective_date)
+
+    # features_targets.py
+    for issuer, cusips in issuer_cusips.iteritems():
+        for cusip in cusips:
+            for fit_date in fit_dates:
+                command(seven.build.features_targets, issuer, cusip, fit_date)
+
+    # fit.py
+    for issuer in issuers:
+        gbi = seven.GetBuildInfo.GetBuildInfo(issuer)
+        cusips_for_build = set(issuer_cusips[issuer])
+        pdb.set_trace()
+        for fit_date in fit_dates:
+            # determine issuepriceids for the cusip on the fit_date
+            issuepriceids = gbi.get_issuepriceids(fit_date)
+            cusips = map(lambda x: gbi.get_cusip(x), issuepriceids)
+            relevant_issuepriceids_cusips = filter(lambda x: x[1] in cusips_for_build, zip(issuepriceids, cusips))
+            print len(relevant_issuepriceids_cusips)
+            for issuepriceid_cusip in relevant_issuepriceids_cusips:
+                issuepriceid, cusip = issuepriceid_cusip
+                command(seven.build.fit, issuer, cusip, str(issuepriceid), hpset)
 
 
 # main program
