0779f9232d707b4899d8bfbcad37f67babfaed73
Roy Lowrance <roy.lowrance@gmail.com>
2017-07-02 09:26:23 -0400

    create targets oasspread and interrarival seconds

--- seven/target_maker.py
+++ seven/target_maker.py
@@ -10,12 +10,11 @@ import seven.InterarrivalTime
 
 class TargetMaker(object):
     def __init__(self):
-        self.data = collections.defaultdict(list)  # Dict[target_name, target_value]
-        self.indices = []
         self.interarrivaltime = collections.defaultdict(lambda: seven.InterarrivalTime.InterarrivalTime())
+        self.targets = pd.DataFrame()  # build this up row by row; its part of the API
 
     def append_targets(self, trace_index, trace_record):
-        'append info to self.data and self.indices; return None or err'
+        'append info to self.data and self.indices; return None (if successful) or err'
         def append_to_data(targets_values):
             for target, value in targets_values.iteritems():
                 self.data[target]. append(value)
@@ -24,38 +23,29 @@ class TargetMaker(object):
         def append_to_indices(trace_index):
             self.indices.append(trace_index)
 
-        iat = self.interarrivaltime[trace_record['cusip']]
-        interarrival_seconds, err = iat.interarrival_seconds(trace_index, trace_record)
-        if err is not None:
-            return 'interarrivaltime: %s' % err
-        targets_values, err = self._make_targets_values(trace_index, trace_record)
+        target_values, err = self._make_targets_values(trace_index, trace_record)
         if err is not None:
             return err
-        targets_values['id_interarrival_seconds'] = interarrival_seconds  # PLAN: will become a target
-        append_to_data(targets_values)
-        append_to_indices(trace_index)
+
+        new_row = pd.DataFrame(data=target_values, index=[trace_index])
+        self.targets = self.targets.append(new_row)
+
         return None
-
-    def get_dataframe(self):
-        'return DataFrame'
-        result = pd.DataFrame(
-            data=self.data,
-            index=self.indices,
-        )
-        return result
-
-    def get_target_dataframe(self):
-        'return DataFrame'
-        # deprecated: used only by fit_predict.py
-        return self.get_targets()
 
     def _make_targets_values(self, trace_index, trace_record):
         'return (dict, err)'
+        iat = self.interarrivaltime[trace_record['cusip']]
+        interarrival_seconds, err = iat.interarrival_seconds(trace_index, trace_record)
+        if err is not None:
+            return None, 'interarrivaltime: %s' % err
+
         oasspread = trace_record['oasspread']
         if np.isnan(oasspread):
             return None, 'oasspread is NaN'
+
         trade_type = trace_record['trade_type']
         assert trade_type in ('B', 'D', 'S')
+
         result = {
             'id_trace_index': trace_index,
             'id_trade_type': trade_type,
@@ -63,11 +53,11 @@ class TargetMaker(object):
             'id_effectivedatetime': trace_record['effectivedatetime'],
             'id_effectivedate': trace_record['effectivedatetime'].date(),
             'id_quantity': trace_record['quantity'],
-            'target_oasspread_B': oasspread if trade_type == 'B' else np.nan,
-            'target_oasspread_D': oasspread if trade_type == 'D' else np.nan,
-            'target_oasspread_S': oasspread if trade_type == 'S' else np.nan,
+            'target_oasspread': oasspread,
+            'target_interarrival_seconds': interarrival_seconds,  # from prrior trade of any trade_type
         }
         return result, None
+
 
 if False:
     pdb
