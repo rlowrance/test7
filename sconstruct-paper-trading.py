221a9a0c56d913d2b351ae67fef3414add35429d
Roy Lowrance <roy.lowrance@gmail.com>
2017-07-09 11:23:25 -0400

    command line to select what to build

--- sconstruct-paper-trading.py
+++ sconstruct-paper-trading.py
@@ -3,6 +3,9 @@
 #   scons -n -f sconstruct.py /
 #   scons --debug=explain -f sconstruct.py /
 #   scons -j <nprocesses> -f sconstruct.py /
+#   scons -j <nprocesses> -f sconstruct.py / what=build
+#   scons -j <nprocesses> -f sconstruct.py / what=features
+#   scons -j <nprocesses> -f sconstruct.py / what=predictions
 
 # where / means to build everything (not just stuff in the current working directory .)
 
@@ -55,116 +58,157 @@ def as_datetime_date(s):
 # We are suggesting the following CUSIPs for Go-Live: (Roy: these are in order)
 # ORCL 68389XAU9
 # ORCL 68389XAS4
-# 459200HU8
+# IBM 459200HU8
 # your AAPL Cusip
-# 023135AN6
+# AMZN 023135AN6
 
-def commands_for():
-    'call command(*args) for appropriate targets'
-    # for now, just work with one CUSIP and hence one issuer
-    CD = collections.namedtuple('CD', 'first_features fit_dates')
-    issuer_cusips = {
-        # 'AAPL': ['037833AG5'],
-        'AMZN': [],
-        'CSCO': [],
-        'GOOGL': [],
-        'MSFT': [],
-        'ORCL': ['68389XAU9'],
-    }
-    cusip_data = {
-        '037833AG5': CD('2017-06-09', ('2017-06-26',)),
-        '68389XAU9': CD('2017-06-15', ('2017-07-05',)),
-    }
-    first_features = {
-        '037833AG5': '2017-06-09',  # for fitting on 2017-06-26
-        '68389XAU9': '2017-06-15',  # a guess, for fitting on 2017-07-05
-    }
-    prediction_dates = {  # NOTE: must be in increasing date order!
-        '037833AG5': ['2017-06-26'],
-        '68389XAU9': ['2017-07-05'],
-    }
-    hpset = 'grid4'
-    # date for which to create features and targets
-    # the oldest such date we need for the select CUSIP is 2017-06-09, to fit trades on 2017-06-26
-    # For AAPL/037833AG5 fitted on 2017006-26, the earliest features needed are on 2017-06-
-    # feature_dates = [  # dates for which to create features and targets
-    #     '2017-06-%02d' % d
-    #     for d in range(9, 28)  # days 09 .. 27
-    # ]
-    # fit_dates = ('2017-06-26',)  # fit  on this Monday, predict on Tuesday
-    hpset = 'grid4'
 
+Dates = collections.namedtuple('Dates', 'first_features first_ensemble last_ensemble')
+issuer_cusips = {
+    #'AAPL': ['037833AG5'],
+    'AMZN': [],
+    'CSCO': [],
+    'GOOGL': [],
+    'MSFT': [],
+    'ORCL': ['68389XAU9', '68389XAS4'],
+}
+dates = {  # by issuer
+    'AAPL': Dates(
+        first_features='2017-06-09',
+        first_ensemble='2017-06-26',
+        last_ensemble='2017-07-06',
+    ),
+    'ORCL': Dates(
+        first_features='2017-06-15',
+        first_ensemble='2017-07-06',
+        last_ensemble='2017-07-06',
+    ),
+}
+
+hpset = 'grid4'
+
+
+def date_range(first, last):
+    'yield consecutive dates in [first, last]'
+    assert first <= last
+    current = first
+    while current <= last:
+        yield current
+        current += datetime.timedelta(1)  # 1 day
+
+
+def ensemble_dates(issuer):
+    'yield each date in [dates[issuer].first_ensemble, dates[issuer].last_ensemble]'
+    first_ensemble_date = as_datetime_date(dates[issuer].first_ensemble)
+    last_ensemble_date = as_datetime_date(dates[issuer].last_ensemble)
+    for date in date_range(first_ensemble_date, last_ensemble_date):
+        yield date
+
+
+def predict_dates(issuer):
+    'yield each date that the experts should make predictions on'
+    # one day preceeding what the ensemble model needs
+    first_predict_date = as_datetime_date(dates[issuer].first_ensemble) - datetime.timedelta(1)
+    last_predict_date = as_datetime_date(dates[issuer].last_ensemble) - datetime.timedelta(1)
+    for date in date_range(first_predict_date, last_predict_date):
+        yield date
+
+
+class TraceInfo(object):
+    def __init__(self):
+        self.infos_by_issuer = {}
+        pass
+
+    def _initialize(self, issuer):
+        'mutate self.infos_by_issuer to hold all of the trace info for the issuer'
+        scons = seven.build.traceinfo(issuer)
+        for k, v in scons.iteritems():
+            # we use just one of the files created by traceinfo.py
+            # NOTE: build.py may use other output files created by traceinfo.py
+            if k == 'out_by_issuer_cusip':
+                with open(v, 'rb') as f:
+                    obj = pickle.load(f)
+                if issuer not in self.infos_by_issuer:
+                    self.infos_by_issuer[issuer] = {}
+                self.infos_by_issuer[issuer][k] = obj
+
+    def infos_for_trades_on(self, issuer, cusip, the_date):
+        'return list of infos on the date for the issuer-cusip'
+        self._initialize(issuer)
+        by_issuer = self.infos_by_issuer[issuer]
+        by_issuer_cusip = by_issuer['out_by_issuer_cusip']
+        infos = by_issuer_cusip[(issuer, cusip)]  # : List[info]
+        result = [
+            info
+            for info in infos
+            if info['effective_date'] == the_date
+        ]
+        return result
+
+    def n_trades_on(self, issuer, cusip, the_date):
+        return len(self.infos_for_trades_on(issuer, cusip, the_date))
+
+    def n_trades_at(self, issuer, cusip, the_datetime):
+        'yield the info on the date'
+        self._initialize(issuer)
+        by_issuer = self.infos_by_issuer[issuer]
+        by_issuer_cusip = by_issuer['out_by_issuer_cusip']
+        infos = by_issuer_cusip[(issuer, cusip)]  # : List[info]
+        count = 0
+        for info in infos:
+            if info['effective_datetime'] == the_datetime:
+                count += 1
+        return count
+
+
+trace_info = TraceInfo()
+
+
+def commands_for_build():
+    'issue command to build the build information'
     # traceinfo.py
-    for issuer, cusips in issuer_cusips.iteritems():
+    for issuer in issuer_cusips:
+        print 'scons traceinfo.py', issuer
         command(seven.build.traceinfo, issuer)
 
-    if False:
-        print 'truncated after traceinfo.py'
-        return
+    # buildinfo.py
+    print 'scons buildinfo.py'
+    command(seven.build.buildinfo)
 
-    # NOTE: read in the previous version of the traceinfo.
-    # FIXME: read the version of the traceinfo created by executing the above command
-    traceinfo_all = {}  # Dict[cusip, traceinfo]
-    traceinfo_by_issuer_cusip = {}  # Dict[cusip, Dict[(issuer, cusip)]]
-    for issuer in issuer_cusips.keys():
-        with open(seven.build.traceinfo(issuer)['out_summary'], 'rb') as f:
-            traceinfo_all[issuer] = pickle.load(f)  # a list of dict
 
-        with open(seven.build.traceinfo(issuer)['out_by_issuer_cusip'], 'rb') as f:
-            traceinfo_by_issuer_cusip[issuer] = pickle.load(f)  # Dict[(issuer,cusip), List[Dict]]
-
-    # features_targets.py
+def commands_for_features():
+    'issue command to build the feature sets'
     for issuer, cusips in issuer_cusips.iteritems():
         for cusip in cusips:
-            cd = cusip_data[cusip]
-            first_feature_date = as_datetime_date(cd.first_features)
-            last_feature_date = as_datetime_date(cd.fit_dates[-1])
-            current_feature_date = first_feature_date
-            while current_feature_date <= last_feature_date:
-                command(seven.build.features_targets, issuer, cusip, current_feature_date)
-                current_feature_date += datetime.timedelta(1)  # 1 day
+            # build feature sets from first date to the last ensemble date
+            first_feature_date = as_datetime_date(dates[issuer].first_features)
+            last_ensemble_date = as_datetime_date(dates[issuer].last_ensemble)
+            for current_date in date_range(first_feature_date, last_ensemble_date):
+                current_date_str = '%s' % current_date
+                print 'scons features_targets.py', issuer, cusip, current_date_str
+                command(seven.build.features_targets, issuer, cusip, current_date_str)
 
-    if False:
-        print 'truncated after features_targets.py'
-        return
 
-    def infos_for_issuer_cusip(issuer, cusip):
-        return traceinfo_by_issuer_cusip[issuer][(issuer, cusip)]
-
-    def infos_for_trades_on(issuer, cusip, date_arg):
-        date = as_datetime_date(date_arg) if isinstance(date_arg, str) else date_arg
-        return filter(
-            lambda info: info['effective_date'] == date,
-            infos_for_issuer_cusip(issuer, cusip),
-        )
-
-    def infos_for_trades_at(issuer, cusip, dt):
-        return filter(
-            lambda info: info['effective_datetime'] == dt,
-            infos_for_issuer_cusip(issuer, cusip),
-        )
-
-    def n_trades_at(issuer, cusip, dt):
-        return len(infos_for_trades_at(issuer, cusip, dt))
-
+def commands_for_predictions():
+    'issue commands to make the ensemble predictions'
+    # fit.py
     for issuer in issuer_cusips.keys():
         for cusip in issuer_cusips[issuer]:
-            cd = cusip_data[cusip]
-            for fit_date in cd.fit_dates:
-                infos_on_fit_date = infos_for_trades_on(issuer, cusip, fit_date)
-                print 'scons fit.py: # trades for %s %s on %s: %d' % (issuer, cusip, fit_date, len(infos_on_fit_date))
-                for info in infos_on_fit_date:
+            for current_date in ensemble_dates(issuer):
+                for info in trace_info.infos_for_trades_on(issuer, cusip, current_date):
+                    issuepriceid = info['issuepriceid']
+                    print 'scons fit.py', issuer, cusip, issuepriceid, hpset
+                    command(seven.build.fit, issuer, cusip, issuepriceid, hpset)
 
-                    command(seven.build.fit, issuer, cusip, str(info['issuepriceid']), hpset)
-
-    if False:
-        print 'truncated after fit.py'
+    if True:
+        print 'WARNING: truncated after fit.py'
         return
 
+    # predict.py
     for issuer in issuer_cusips.keys():
         for cusip in issuer_cusips[issuer]:
-            for prediction_date in prediction_dates[cusip]:
-                predict_trades = infos_for_trades_on(issuer, cusip, prediction_date)
+            for prediction_date in predict_dates(issuer):
+                predict_trades = trace_info.infos_for_trades_on(issuer, cusip, prediction_date)
                 print 'scons predict.py: num trades for %s %s on %s: %d' % (
                     issuer,
                     cusip,
@@ -187,7 +231,7 @@ def commands_for():
                         reverse=True,
                     )
                     for previous_trade in sorted_previous_trades:
-                        n_trades = n_trades_at(issuer, cusip, previous_trade['effective_datetime'])
+                        n_trades = trace_info.n_trades_at(issuer, cusip, previous_trade['effective_datetime'])
                         print '    previous trade issueprice id %s at datetime %s; %d trades at that datetime' % (
                             previous_trade['issuepriceid'],
                             previous_trade['effective_datetime'],
@@ -203,27 +247,47 @@ def commands_for():
                             break  # stop the search
                         # continue to search backwards in time
 
-    if True:
-        print 'truncated after predict.py'
+    if False:
+        print 'WARNING: truncated after predict.py'
         return
 
     # accuracy.py
-    accuracy_issuer = 'AAPL'
-    accuracy_cusip = '037833AG5'
-    accuracy_dates = ('2017-06-26',)
-    for accuracy_date in accuracy_dates:
-        command(seven.build.accuracy, accuracy_issuer, accuracy_cusip, accuracy_date)
+    for issuer in issuer_cusips.keys():
+        for cusip in issuer_cusips[issuer]:
+            for prediction_date in predict_dates(issuer):
+                if trace_info.n_trades_on(issuer, cusip, prediction_date) > 0:
+                    print 'scons accuracy.py', issuer, cusip, prediction_date
+                    command(seven.build.accuracy, issuer, cusip, str(prediction_date))
 
     if False:
-        print 'truncated after accuracy.py'
+        print 'WARNING: truncated after accuracy.py'
         return
 
     # ensemble_predictions.py
-    ensemble_issuer = 'AAPL'
-    ensemble_cusip = '037833AG5'
-    ensemble_dates = ('2017-06-27',)
-    for ensemble_date in ensemble_dates:
-        command(seven.build.ensemble_predictions, ensemble_issuer, ensemble_cusip, ensemble_date)
+    for issue in issuer_cusips.keys():
+        for cusip in issuer_cusips[issuer]:
+            for ensemble_date in ensemble_dates(issuer):
+                print 'scons ensemble_predictions', issuer, cusip, ensemble_date
+                command(seven.build.ensemble_predictions, issuer, cusip, str(ensemble_date))
 
+
+##############################################################################################
 # main program
-commands_for()
+##############################################################################################
+def invocation_error(msg=None):
+    if msg is not None:
+        print 'ERROR: %s' % msg
+    print 'ERROR: must specify what=[build | features | predictions] on invocation'
+
+
+what = ARGUMENTS.get('what', None)
+if what == 'None':
+    invocation_error()
+elif what == 'build':
+    commands_for_build()
+elif what == 'features':
+    commands_for_features()
+elif what == 'predictions':
+    commands_for_predictions()
+else:
+    invocation_error('what=%s is not a recognized invocation option' % what)    
