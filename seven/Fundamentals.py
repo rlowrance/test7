ac4eac18ba0806bea669609067ea478f7995a1f8
Roy Lowrance <roy.lowrance@gmail.com>
2017-07-22 14:54:15 -0400

    deprate in favor of equivalent functionality in feature_makers.py

--- seven/Fundamentals.py
+++ seven/Fundamentals.py
@@ -14,6 +14,7 @@ import read_csv  # imports seven/read_csv
 
 class Fundamentals(object):
     def __init__(self, issuer):
+        assert Fail, 'deprecated: use feature_makers.Fundamentals instead'
         self.issuer = issuer
         self.file_logical_names = (  # feature names are the same as the logical names
             'expected_interest_coverage',
