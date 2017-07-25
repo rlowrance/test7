9d2f7bb40336671da0d7ae1ff1e2b2ced2ec643b
Roy Lowrance <roy.lowrance@gmail.com>
2017-07-25 15:33:06 -0400

    add exceptions CriticalException, ErrorException

--- seven/exception.py
+++ seven/exception.py
@@ -4,3 +4,19 @@ class BuildException(Exception):
         super(BuildException, self).__init__(msg)
         self.msg = msg
         print 'raising BuildException:', msg
+
+
+class CriticalException(Exception):
+    'raised by seven.logging.error, if not debugging'
+    def __init__(self, msg):
+        super(CriticalException, self).__init__(msg)
+        self.msg = msg
+        print 'raising BuildException:', msg
+
+
+class ErrorException(Exception):
+    'raised by seven.logging.error, if not debugging'
+    def __init__(self, msg):
+        super(ErrorException, self).__init__(msg)
+        self.msg = msg
+        print 'raising BuildException:', msg
