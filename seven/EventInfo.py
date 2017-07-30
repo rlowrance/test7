2bc0c3797833d774907dc926a4fefca976f3b05a
Roy Lowrance <roy.lowrance@gmail.com>
2017-07-30 06:46:30 -0400

    alphabetically order methods

--- seven/EventInfo.py
+++ seven/EventInfo.py
@@ -77,15 +77,57 @@ class EventInfo(object):
     def __repr__(self):
         return 'EventInfo(%s, %s)' % (self.issuer, self.cusip)
 
-    def sorted_events_at_datetime(self, when_datetime):
-        'return list of events at the datetime'
+    def at_datetime(self, when_datetime):
+        'list of paths to all events at the datetime'
         assert isinstance(when_datetime, datetime.datetime)
-        return self._sorted_events_at_datetime[when_datetime]
+        return self._at_datetime[when_datetime]
 
-    def sorted_events_on_date(self, when_date):
-        'return possibly empty list of events on the date in ascending datetime order'
-        assert isinstance(when_date, datetime.date)
-        return self._sorted_events_on_date.get(when_date, [])
+    def just_prior_datetime(self, event_id):
+        'return datetime just prior to the event_id'
+        assert isinstance(event_id, EventId.EventId)        
+        if event_id not in self._event_ids:
+            raise exception.EventInfoException('event id %s is not in the file system' % event_id)
+        this_datetime = event_id.datetime()
+        for i, dt in self._ascending_event_datetimes():
+            if dt == this_datetime:
+                if i == 0:
+                    print 'no event prior to', this_datetime
+                    pdb.set_trace()
+                else:
+                    return self.event_datetimes[i - 1]
+        logging.critical('logic error')
+
+    def just_prior_distinct_datetime_event(self, query_event_id, reclassified_trade_type):
+        'return the oldest prior event that has a distinct datetime and same reclassified trade type'
+        assert isinstance(query_event_id, EventId.EventId)
+        assert reclassified_trade_type in ('B', 'S')
+        for prior_datetime in reversed(self.sorted_prior_datetimes(query_event_id.datetime())):
+            prior_event_ids = self.sorted_events_at_datetime(prior_datetime)
+            if len(prior_event_ids) == 1:
+                prior_event_id = prior_event_ids[0]
+                if self.reclassified_trade_type(prior_event_id) == reclassified_trade_type:
+                    return prior_event_id
+        raise exception.EventInfoException(
+            'no prior event with a distinct datetime and trade type %s for event %s' % (
+                reclassified_trade_type,
+                query_event_id,
+                )
+        )
+
+    def just_prior_distinct_datetime_event_on(self, reclassified_trade_type, trade_date):
+        'return the oldest prior event that has a distinct datetime and same reclassified trade type'
+        assert reclassified_trade_type in ('B', 'S')
+        assert isinstance(trade_date, datetime.date)
+        for event_id in list(reversed(self.sorted_events_on_date(trade_date))):
+            if len(self.sorted_events_at_datetime(event_id.datetime())) == 1:
+                if self.reclassified_trade_type(event_id) == reclassified_trade_type:
+                    return event_id
+        raise exception.EventInfoException(
+            'no prior event with a distinct datetime and trade type %s on date %s' % (
+                reclassified_trade_type,
+                trade_date,
+                )
+        )
 
     def path_to_features(self, event_id):
         'return path in file system to the features for the event_id'
@@ -95,6 +137,16 @@ class EventInfo(object):
         else:
             print event_id, type(event_id)
             raise exception.EventInfoException('event id %s is not in the file system' % event_id)
+
+    def paths_to_features_on_date(self, when_date):
+        'list of paths to all events on the date'
+        assert isinstance(when_date, datetime.date)
+        result = []
+        for event_id in self._sorted_events_on_date[when_date]:
+            if event_id.date() == when_date:
+                result.append(self._path_to_event[event_id])
+        pdb.set_trace()
+        return result
 
     def path_to_fitted_dir(self, event_id, target):
         assert isinstance(event_id, EventId.EventId)
@@ -129,67 +181,15 @@ class EventInfo(object):
         else:
             raise exception.EventInfoException('event id %s is not in the file system' % event_id)
 
-    def at_datetime(self, when_datetime):
-        'list of paths to all events at the datetime'
-        assert isinstance(when_datetime, datetime.datetime)
-        return self._at_datetime[when_datetime]
-
-    def paths_to_features_on_date(self, when_date):
-        'list of paths to all events on the date'
+    def sorted_events_on_date(self, when_date):
+        'return possibly empty list of events on the date in ascending datetime order'
         assert isinstance(when_date, datetime.date)
-        result = []
-        for event_id in self._sorted_events_on_date[when_date]:
-            if event_id.date() == when_date:
-                result.append(self._path_to_event[event_id])
-        pdb.set_trace()
-        return result
+        return self._sorted_events_on_date.get(when_date, [])
 
-    def just_prior_datetime(self, event_id):
-        'return datetime just prior to the event_id'
-        assert isinstance(event_id, EventId.EventId)        
-        if event_id not in self._event_ids:
-            raise exception.EventInfoException('event id %s is not in the file system' % event_id)
-        this_datetime = event_id.datetime()
-        for i, dt in self._ascending_event_datetimes():
-            if dt == this_datetime:
-                if i == 0:
-                    print 'no event prior to', this_datetime
-                    pdb.set_trace()
-                else:
-                    return self.event_datetimes[i - 1]
-        logging.critical('logic error')
-
-    def just_prior_distinct_datetime_event_on(self, reclassified_trade_type, trade_date):
-        'return the oldest prior event that has a distinct datetime and same reclassified trade type'
-        assert reclassified_trade_type in ('B', 'S')
-        assert isinstance(trade_date, datetime.date)
-        for event_id in list(reversed(self.sorted_events_on_date(trade_date))):
-            if len(self.sorted_events_at_datetime(event_id.datetime())) == 1:
-                if self.reclassified_trade_type(event_id) == reclassified_trade_type:
-                    return event_id
-        raise exception.EventInfoException(
-            'no prior event with a distinct datetime and trade type %s on date %s' % (
-                reclassified_trade_type,
-                trade_date,
-                )
-        )
-
-    def just_prior_distinct_datetime_event(self, query_event_id, reclassified_trade_type):
-        'return the oldest prior event that has a distinct datetime and same reclassified trade type'
-        assert isinstance(query_event_id, EventId.EventId)
-        assert reclassified_trade_type in ('B', 'S')
-        for prior_datetime in reversed(self.sorted_prior_datetimes(query_event_id.datetime())):
-            prior_event_ids = self.sorted_events_at_datetime(prior_datetime)
-            if len(prior_event_ids) == 1:
-                prior_event_id = prior_event_ids[0]
-                if self.reclassified_trade_type(prior_event_id) == reclassified_trade_type:
-                    return prior_event_id
-        raise exception.EventInfoException(
-            'no prior event with a distinct datetime and trade type %s for event %s' % (
-                reclassified_trade_type,
-                query_event_id,
-                )
-        )
+    def sorted_events_at_datetime(self, when_datetime):
+        'return list of events at the datetime'
+        assert isinstance(when_datetime, datetime.datetime)
+        return self._sorted_events_at_datetime[when_datetime]
 
     def sorted_prior_datetimes(self, starting_datetime):
         'return list of prior datetime in ascending datetime order'
