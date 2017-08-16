eb61e4d4d5b9d9c6fdb3fd973373a4d772430580
Roy Lowrance <roy.lowrance@gmail.com>
2017-08-16 15:42:15 -0400

    rm event_id arg type

--- seven/arg_type.py
+++ seven/arg_type.py
@@ -10,13 +10,10 @@ You may not use this file except in compliance with a License.
 '''
 
 import argparse
-import collections
 import datetime
 import multiprocessing
 import os
 import pdb
-
-import EventId
 
 if False:
     pdb
@@ -46,14 +43,6 @@ def date(s):
         return s
     except:
         raise argparse.ArgumentError('%is is not a date in form YYYY-MM-DD' % s)
-
-
-def event_id(s):
-    try:
-        EventId.EventId.from_str(s)
-        return s
-    except:
-        raise argparse.ArgumentError('%s is not an event id YYYY-MM-DD-HH-MM-SS-source-source_id' % s)
 
 
 def filename_csv(s):
@@ -103,14 +92,6 @@ def _in_set(s, allowed):
 
 def target(s):
     return _in_set(s, ['oasspread'])
-
-
-def ticker(s):
-    try:
-        assert len(s.split('.')) == 1
-        return s
-    except:
-        raise argparse.ArgumentTypeError('%s is not a ticker' % s)
 
 
 def trade_id(s):
