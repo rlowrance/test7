4366d1196ecc8c28171adf5c3dbcd0f150ccbb45
Roy Lowrance <roy.lowrance@gmail.com>
2017-08-23 13:10:04 -0400

    do not visit invalid directories

--- seven/WalkTestTrainOutputDirectories.py
+++ seven/WalkTestTrainOutputDirectories.py
@@ -1,5 +1,7 @@
 'visit every output directory created by test_train.py'
+import datetime
 import os
+import pdb
 
 
 class WalkTestTrainOutputDirectories(object):
@@ -17,10 +19,19 @@ class WalkTestTrainOutputDirectories(object):
                     for hpset in os.listdir(path_target):
                         path_hpset = os.path.join(path_target, hpset)
                         for start_events in os.listdir(path_hpset):
+                            if not self._is_date(start_events):
+                                # skip directories without a date
+                                # these are possibly left-over from prior versions of the application
+                                # in any case, they were not created by test_train.py
+                                continue
                             path_startevents = os.path.join(path_hpset, start_events)
                             for start_predictions in os.listdir(path_startevents):
+                                if not self._is_date(start_predictions):
+                                    continue
                                 path_startpredictions = os.path.join(path_startevents, start_predictions)
                                 for stop_predictions in os.listdir(path_startpredictions):
+                                    if not self._is_date(stop_predictions):
+                                        continue
                                     path_stoppredictions = os.path.join(path_startpredictions, stop_predictions)
                                     invocation_parameters = {
                                         'issuer': issuer,
@@ -34,3 +45,16 @@ class WalkTestTrainOutputDirectories(object):
                                         directory_path=path_stoppredictions,
                                         invocation_parameters=invocation_parameters,
                                     )
+
+    def _is_date(self, s):
+        'does string s contain a valid date?'
+        try:
+            year, month, day = s.split('-')
+            datetime.date(int(year), int(month), int(day))
+            return True
+        except Exception:
+            return False
+
+
+if __name__ == '__main__':
+    pdb
