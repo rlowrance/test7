31532e7149a54ec84d7dea27f758d8fd9f9bd024
Roy Lowrance <roy.lowrance@gmail.com>
2017-09-19 08:31:07 -0400

    temp runner to rebuild version 1 features

---
+++ go.bat
@@ -0,0 +1,7 @@
+rem rebuild feature version 1 test_train outputs
+python daily.py 2017-09-11 16 1 1 --debug --debugtesttrain
+python daily.py 2017-09-12 16 1 1 --debug --debugtesttrain
+python daily.py 2017-09-13 16 1 1 --debug --debugtesttrain
+python daily.py 2017-09-14 16 1 1 --debug --debugtesttrain
+python daily.py 2017-09-15 16 1 1 --debug --debugtesttrain
+python daily.py 2017-09-18 16 1 1 --debug --debugtesttrain
