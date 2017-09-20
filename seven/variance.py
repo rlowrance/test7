94f98922632c8f48bde54c63d8f86341d64ee497
Roy Lowrance <roy.lowrance@gmail.com>
2017-09-20 16:54:45 -0400

    further development

--- seven/variance.py
+++ seven/variance.py
@@ -8,6 +8,7 @@ INVOCATION TO RUN UNIT TESTS
 Copyright 2017 Roy E. Lowrance, roy.lowrance@gmail.com
 You may not use this file except in compliance with a License.
 '''
+import collections
 import pdb
 import unittest
 
@@ -18,6 +19,18 @@ class Online:
         self._n = 0
         self._mean = 0.0
         self._m2 = 0.0
+        self._recent = collections.deque(maxlen=10)
+
+    def __repr__(self):
+        if self.has_values():
+            return 'Online(%d, %0.2f, %0.2f)[%s]' % (
+                self._n,
+                self.get_mean(),
+                self.get_sample_variance(),
+                self._recent,
+            )
+        else:
+            return 'Online(%d)' % self._n
 
     def update(self, x: float) -> None:
         self._n += 1
@@ -25,7 +38,14 @@ class Online:
         self._mean += delta / self._n
         delta2 = x - self._mean
         self._m2 += delta * delta2
+        self._recent.append(x)
 
+    def has_values(self):
+        return self._n >= 2
+
+    def get_n(self) -> int:
+        return self._n
+    
     def get_mean(self) -> float:
         return float('nan') if self._n < 2 else self._mean
 
