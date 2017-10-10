0c6104301cea029b991cec92281c0c3a01a0cd4d
Roy Lowrance <roy.lowrance@7-chord.com>
2017-10-10 13:30:01 +0000

    cp from seven/

---
+++ debug.py
@@ -0,0 +1,40 @@
+'''enter debugger on error
+
+usage:
+ import debug
+
+ ref: http://stackoverflow.com/questions/242485/starting-python-debugger-automatically-on-error
+
+Copyright 2017 Roy E. Lowrance
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on as "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing premission and
+limitation under the license.
+'''
+import sys
+
+
+def info(type, value, tb):
+    if hasattr(sys, 'ps1') or not sys.stderr.isatty():
+        # we are in interactive mode or we don't have a tty-like
+        # device, so we call the default hook
+        sys.__excepthook__(type, value, tb)
+    else:
+        import traceback, pdb
+        # we are NOT in interactive mode, print the exception...
+        traceback.print_exception(type, value, tb)
+        print()
+        # ...then start the debugger in post-mortem mode.
+        # pdb.pm() # deprecated
+        pdb.post_mortem(tb)  # more "modern"
+
+
+sys.excepthook = info
